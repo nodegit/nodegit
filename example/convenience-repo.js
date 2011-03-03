c6851563b8160262d5c74e07410e5053779228eb
Tim Branyen <tim@tabdeveloper.com>
2011-03-03 11:58:08 -0500

    Cleaned up errors api

--- example/convenience-repo.js
+++ example/convenience-repo.js
@@ -5,17 +5,17 @@ git.repo( './dummyrepo/.git', function( err, path, repo ) {
   if( err ) { throw err; }
  
   // Read a commit
-  repo.commit( '2f6cbe055f1a6ca0a3ba524ba88a7806ba507a89', function( err, commit ) {
-    if( err ) { throw err; }
+  //repo.commit( '2f6cbe055f1a6ca0a3ba524ba88a7806ba507a89', function( err, commit ) {
+  //  if( err ) { throw err; }
 
-    console.log( commit.msg() );
+  //  console.log( commit.msg() );
 
-    var author = commit.author();
-    console.log( author.name );
-    console.log( author.email );
-  });
+  //  var author = commit.author();
+  //  console.log( author.name );
+  //  console.log( author.email );
+  //});
 
   repo.head( 'master', function( err, path, head ) {
-    git.commit( head ).author().name;
+    console.log( git.error( err ) );
   });
 });
