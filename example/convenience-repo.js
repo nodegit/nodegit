b2819b70167e749abfa0a0f052ecf97dcc91b7e4
tbranyen <tim@tabdeveloper.com>
2011-04-22 11:05:06 -0400

    Updated convenience-repo.js

--- example/convenience-repo.js
+++ example/convenience-repo.js
@@ -8,7 +8,7 @@ git.repo( '../.git', function( err, repo ) {
   repo.branch( 'master', function( err, branch ) {
     if( err ) { throw err; }
     // Iterate over the revision history
-    branch.history.each( function( i, commit ) {
+    branch.history().on( 'commit', function( err, commit ) {
       // Print out `git log` emulation
       console.log( 'commit ' + commit.sha );
       console.log( commit.author.name + ' <' + commit.author.email + '>' );
