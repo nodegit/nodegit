a3c5009970cce399c23af3fa9917a8811d0691c6
Michael Robinson <mike@pagesofinterest.net>
2013-03-02 02:04:23 +1300

    Replaced " with ', updated test

--- test/convenience-repo.js
+++ test/convenience-repo.js
@@ -1,17 +1,17 @@
-var git = require( "../" );
-var rimraf = require("rimraf");
-var fs = require( "fs" );
+var git = require( '../' );
+var rimraf = require('rimraf');
+var fs = require( 'fs' );
 
 // Helper functions
 var helper = {
   // Test if obj is a true function
   testFunction: function(test, obj, label) {
     // The object reports itself as a function
-    test(typeof obj, "function", label + " reports as a function.");
+    test(typeof obj, 'function', label + ' reports as a function.');
     // This ensures the repo is actually a derivative of the Function [[Class]]
-    test(toString.call(obj), "[object Function]", label + " [[Class]] is of type function.");
+    test(toString.call(obj), '[object Function]', label + ' [[Class]] is of type function.');
   },
-  // Test code and handle exception thrown 
+  // Test code and handle exception thrown
   testException: function(test, fun, label) {
     try {
       fun();
@@ -29,20 +29,20 @@ var helper = {
 exports.method = function(test){
   test.expect(5);
 
-  helper.testFunction(test.equals, git.repo, "Repo");
+  helper.testFunction(test.equals, git.repo, 'Repo');
 
   // Test callback argument existence
   helper.testException(test.ok, function() {
-    git.repo("some/path");
-  }, "Throw an exception if no callback");
+    git.repo('some/path');
+  }, 'Throw an exception if no callback');
 
   // Test invalid repository
-  git.repo("/etc/hosts", function(err, path) {
-    test.equals("The specified repository is invalid", err, "Invalid repository error code");
+  git.repo('/etc/hosts', function(err, path) {
+    test.equals('The specified repository is invalid', err.message, 'Invalid repository error code');
 
     // Test valid repository
-    git.repo("../.git", function(err, path) {
-      test.equals(0, err, "Valid repository error code");
+    git.repo('../.git', function(err, path) {
+      test.equals(0, err, 'Valid repository error code');
 
       test.done();
     });
@@ -56,19 +56,19 @@ exports.method = function(test){
 exports.init = function(test) {
   test.expect(4);
 
-  helper.testFunction(test.equals, git.repo().init, "Repo::Init");
+  helper.testFunction(test.equals, git.repo().init, 'Repo::Init');
 
   // Cleanup, remove test repo directory - if it exists
-  rimraf("./test.git", function() {
+  rimraf('./test.git', function() {
     // Create bare repo and test for creation
-    git.repo().init("./test.git", true, function(err, path, isBare) {
-      test.equals(0, err, "Successfully created bare repository");
+    git.repo().init('./test.git', true, function(err, path, isBare) {
+      test.equals(0, err, 'Successfully created bare repository');
       // Verify repo exists
-      git.repo("./test.git", function(err, path, repo) {
-        test.equals(0, err, "Valid repository created");
+      git.repo('./test.git', function(err, path, repo) {
+        test.equals(0, err, 'Valid repository created');
 
         // Cleanup, remove test repo directory
-        rimraf("./test.git", test.done);
+        rimraf('./test.git', test.done);
       });
     });
   });
