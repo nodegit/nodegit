8100692c66c6aba456d1b38230005def532f94be
Michael Robinson <mike@pagesofinterest.net>
2013-03-09 23:48:58 +1300

    Whitespace

--- test/raw-commit.js
+++ test/raw-commit.js
@@ -13,7 +13,7 @@ var helper = {
     // This ensures the repo is actually a derivative of the Function [[Class]]
     test( toString.call( obj ), '[object Function]', label +' [[Class]] is of type function.' );
   },
-  // Test code and handle exception thrown 
+  // Test code and handle exception thrown
   testException: function( test, fun, label ) {
     try {
       fun();
@@ -31,7 +31,7 @@ exports.constructor = function( test ){
 
   // Test for function
   helper.testFunction( test.equals, git.Commit, 'Commit' );
-  
+
   testRepo.open( path.resolve( '../.git' ), function( err ) {
     // Ensure we get an instance of Commit
     test.ok( new git.Commit( testRepo ) instanceof git.Commit, 'Invocation returns an instance of Commit' );
@@ -47,10 +47,10 @@ exports.lookup = function( test ) {
 
   testOid.mkstr( 'cb09e99e91d41705197e0fb60823fdc7df776691' );
 
-  test.expect( 8 );
+  test.expect(8);
 
   // Test for function
-  helper.testFunction( test.equals, testCommit.lookup, 'Commit::Lookup' );
+  helper.testFunction(test.equals, testCommit.lookup, 'Commit::Lookup');
 
   // Test repo argument existence
   helper.testException( test.ok, function() {
@@ -77,11 +77,11 @@ exports.lookup = function( test ) {
     testOid.mkstr( '100644' );
     testCommit.lookup( testRepo, testOid, function( err ) {
       test.notEqual( 0, err, 'Not a valid commit' );
- 
+
       // Test valid commit
       testOid.mkstr( 'cb76e3c030ab29db332aff3b297dc39451a84762' );
       testCommit.lookup( testRepo, testOid, function( err ) {
-        test.equals( 0, err, 'Valid commit');
+        test.equals(null, err, 'Valid commit');
 
         //test.equals( 'Updated gitignore and raw-commit test', testCommit.messageShort(), 'Commit message is valid' );
 
