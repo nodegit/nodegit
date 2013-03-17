b9885fc4dccbcb1eb2f36558bb3d6cbb18c49d03
Michael Robinson <mike@pagesofinterest.net>
2013-03-18 00:55:35 +1300

    Added delta structs

--- include/diff_list.h
+++ include/diff_list.h
@@ -9,6 +9,7 @@
 #include <node.h>
 #include <vector>
 #include <string>
+#include <map>
 
 #include "../vendor/libgit2/include/git2.h"
 
@@ -27,6 +28,8 @@ class GitDiffList : public ObjectWrap {
      * v8::FunctionTemplate used to create Node.js constructor
      */
     static Persistent<Function> constructor_template;
+
+    static const int WALK_DELTA_THRESHHOLD = 10;
 
     /**
      * Used to intialize the EventEmitter from Node.js
@@ -50,8 +53,31 @@ class GitDiffList : public ObjectWrap {
     static void TreeToTreeWork(uv_work_t *req);
     static void TreeToTreeAfterWork(uv_work_t *req);
 
-    static Handle<Value> Iterate(const Arguments& args);
-    static void IterateWork(void *arg);
+    /**
+     * Walk the current git_diff_list
+     */
+    static Handle<Value> Walk(const Arguments& args);
+    static void WalkWork(void *payload);
+    static int WalkWorkFile(const git_diff_delta *delta, float progress,
+                             void *payload);
+    static int WalkWorkHunk(const git_diff_delta *delta, const git_diff_range *range,
+                             const char *header, size_t header_len, void *payload);
+    static int WalkWorkData(const git_diff_delta *delta, /** delta that contains this data */
+                             const git_diff_range *range, /** range of lines containing this data */
+                             char line_origin,            /** git_diff_list_t value from above */
+                             const char *content,         /** diff data - not NUL terminated */
+                             size_t content_len,          /** number of bytes of diff data */
+                             void *payload);              /** user reference data */
+    /**
+     * Called when WalkWorkFile reaches its cache
+     * thresholds. Passes data back to main thread
+     *
+     * @param payload The WalkBaton
+     */
+    static void WalkWorkSendFile(uv_async_t *handle, int status /*UNUSED*/);
+    static void WalkWorkSendHunk(uv_async_t *handle, int status /*UNUSED*/);
+    static void WalkWorkSendData(uv_async_t *handle, int status /*UNUSED*/);
+    static void WalkWorkSendEnd(uv_async_t *handle, int status /*UNUSED*/);
 
   private:
     git_diff_list* diffList;
@@ -74,9 +100,29 @@ class GitDiffList : public ObjectWrap {
       Persistent<Function> callback;
     };
 
-    struct IterateBaton {
+    struct DeltaContent {
+      git_diff_range *range;
+      char lineOrigin;
+      size_t contentLength;
+      std::string content;
+    };
+
+    struct Delta {
+      git_diff_delta *raw;
+      std::vector<DeltaContent* > contents;
+    };
+
+    struct WalkBaton {
       uv_thread_t threadId;
+      uv_mutex_t mutex;
+      uv_async_t asyncFile;
+      uv_async_t asyncHunk;
+      uv_async_t asyncData;
+      uv_async_t asyncEnd;
+
       const git_error* error;
+
+      std::map<std::string, Delta* > fileDeltas;
 
       git_diff_list* rawDiffList;
       Persistent<Function> fileCallback;
