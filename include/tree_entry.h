4051efcb0481881bc73b38f7e9e788cf645e1094
Michael Robinson <mike@pagesofinterest.net>
2013-03-28 00:00:50 +1300

    Added toBlob to tree_entry

--- include/tree_entry.h
+++ include/tree_entry.h
@@ -43,8 +43,23 @@ class GitTreeEntry : ObjectWrap {
   protected:
     static Handle<Value> New(const Arguments& args);
 
+    static Handle<Value> ToBlob(const Arguments& args);
+    static void ToBlobWork(uv_work_t *req);
+    static void ToBlobAfterWork(uv_work_t *req);
+
   private:
     git_tree_entry* entry;
+
+    struct ToBlobBaton {
+      uv_work_t request;
+      const git_error* error;
+
+      git_repository* rawRepo;
+      git_tree_entry* rawEntry;
+      git_blob* rawBlob;
+
+      Persistent<Function> callback;
+    };
 };
 
 #endif
--- src/tree_entry.cc
+++ src/tree_entry.cc
@@ -13,6 +13,7 @@ Copyright (c) 2011, Tim Branyen @tbranyen <tim@tabdeveloper.com>
 #include "../include/object.h"
 #include "../include/oid.h"
 #include "../include/tree_entry.h"
+#include "../include/error.h"
 
 using namespace v8;
 using namespace node;
@@ -22,6 +23,8 @@ void GitTreeEntry::Initialize(Handle<v8::Object> target) {
 
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
   tpl->SetClassName(String::NewSymbol("TreeEntry"));
+
+  NODE_SET_PROTOTYPE_METHOD(tpl, "toBlob", ToBlob);
 
   constructor_template = Persistent<Function>::New(tpl->GetFunction());
   target->Set(String::NewSymbol("TreeEntry"), constructor_template);
@@ -45,6 +48,70 @@ Handle<Value> GitTreeEntry::New(const Arguments& args) {
   return scope.Close(args.This());
 }
 
+Handle<Value> GitTreeEntry::ToBlob(const Arguments& args) {
+  HandleScope scope;
+
+  if(args.Length() == 0 || !args[0]->IsObject()) {
+    return ThrowException(Exception::Error(String::New("Repository is required and must be an Object.")));
+  }
+
+  if(args.Length() == 1 || !args[1]->IsFunction()) {
+    return ThrowException(Exception::Error(String::New("Callback is required and must be a Function.")));
+  }
+
+  ToBlobBaton* baton = new ToBlobBaton();
+  baton->request.data = baton;
+  baton->error = NULL;
+  baton->rawRepo = ObjectWrap::Unwrap<GitRepo>(args[0]->ToObject())->GetValue();
+  baton->rawEntry = ObjectWrap::Unwrap<GitTreeEntry>(args.This())->GetValue();
+  baton->rawBlob = NULL;
+  baton->callback = Persistent<Function>::New(Local<Function>::Cast(args[1]));
+
+  uv_queue_work(uv_default_loop(), &baton->request, ToBlobWork, (uv_after_work_cb)ToBlobAfterWork);
+
+  return Undefined();
+}
+void GitTreeEntry::ToBlobWork(uv_work_t *req) {
+  ToBlobBaton* baton = static_cast<ToBlobBaton*>(req->data);
+
+  int returnCode = git_tree_entry_to_object((git_object**)&baton->rawBlob, baton->rawRepo, baton->rawEntry);
+  if (returnCode != GIT_OK) {
+    baton->error = giterr_last();
+  }
+}
+void GitTreeEntry::ToBlobAfterWork(uv_work_t *req) {
+  HandleScope scope;
+  ToBlobBaton* baton = static_cast<ToBlobBaton* >(req->data);
+
+  if (baton->error) {
+    Local<Value> argv[1] = {
+      GitError::WrapError(baton->error)
+    };
+
+    TryCatch try_catch;
+    baton->callback->Call(Context::GetCurrent()->Global(), 1, argv);
+    if (try_catch.HasCaught()) {
+      node::FatalException(try_catch);
+    }
+  } else {
+
+    Handle<Object> blob = GitBlob::constructor_template->NewInstance();
+    GitBlob *blobInstance = ObjectWrap::Unwrap<GitBlob>(blob);
+    blobInstance->SetValue(baton->rawBlob);
+
+    Handle<Value> argv[2] = {
+      Local<Value>::New(Null()),
+      blob
+    };
+
+    TryCatch try_catch;
+    baton->callback->Call(Context::GetCurrent()->Global(), 2, argv);
+    if (try_catch.HasCaught()) {
+      node::FatalException(try_catch);
+    }
+  }
+  delete req;
+}
 
 Persistent<Function> GitTreeEntry::constructor_template;
 
