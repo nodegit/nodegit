803ea049c9bbd453d8655afd388c30c10a658536
Michael Robinson <mike@pagesofinterest.net>
2013-04-02 22:16:48 +1300

    Updated convenience-tree tests

--- test/convenience-tree.js
+++ test/convenience-tree.js
@@ -9,19 +9,18 @@ exports.walk = function(test) {
   test.expect(516);
 
   git.repo('../.git', function(error, repo) {
-    if(error) { throw error; }
-      // @todo assert repo is correct
       repo.commit(sha, function(error, commit) {
-        if(error) { throw error; }
         var entryCount = 0;
-        commit.tree().walk().on('entry', function(error, index, entry) {
+        commit.tree(function(error, tree) {
+          tree.walk().on('entry', function(error, index, entry) {
+              test.equals(error, null, 'There should be no error');
+              entryCount++;
+          }).on('end', function(error, entries) {
             test.equals(error, null, 'There should be no error');
-            entryCount++;
-        }).on('end', function(error, entries) {
-          test.equals(error, null, 'There should be no error');
-          test.equals(entryCount, fileCount, 'The manual tree entry count and the "end" tree entry count do not match');
-          test.equals(entries.length, fileCount, 'The end entries count and the manual entry count do not match');
-          test.done();
+            test.equals(entryCount, fileCount, 'The manual tree entry count and the "end" tree entry count do not match');
+            test.equals(entries.length, fileCount, 'The end entries count and the manual entry count do not match');
+            test.done();
+          });
         });
       });
    });
