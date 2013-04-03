a811231d505711204368c63d645bf743916e614b
Michael Robinson <mike@pagesofinterest.net>
2013-04-03 23:45:38 +1300

    Tidied error.h

--- include/error.h
+++ include/error.h
@@ -16,48 +16,23 @@ using namespace v8;
 using namespace node;
 
 /**
- * Class: GitError
  *   Wrapper for libgit2 git_error.
  */
 class GitError : public ObjectWrap {
   public:
-    /**
-     * Variable: constructor_template
-     *   Used to create Node.js constructor.
-     */
+
     static Persistent<Function> constructor_template;
-    /**
-     * Function: Initialize
-     *   Used to intialize the EventEmitter from Node.js
-     *
-     * Parameters:
-     *   target - Object the Node.js global module object
-     */
+
     static void Initialize(Handle<Object> target);
 
     static Local<Object> WrapError(const git_error* error);
 
   protected:
-    const git_error* error;
-
-    /**
-     * Constructor: GitError
-     */
     GitError() {};
-    /**
-     * Deconstructor: GitError
-     */
     ~GitError() {};
 
-    /**
-     * Function: New
-     *
-     * Parameters:
-     *   args Arguments function call
-     *
-     * Returns:
-     *   Object args.This()
-     */
+    const git_error* error;
+
     static Handle<Value> New(const Arguments& args);
 };
 
