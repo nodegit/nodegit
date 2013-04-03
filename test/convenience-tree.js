5e63a2fbb25d95e3c5a7f71c518cc2b55ae8d3b8
Michael Robinson <mike@pagesofinterest.net>
2013-04-03 23:44:37 +1300

    Updated convenience-tree test

--- test/convenience-tree.js
+++ test/convenience-tree.js
@@ -3,25 +3,25 @@ var git = require('../'),
     fs = require('fs');
 
 var sha = '5716e9757886eaf38d51c86b192258c960d9cfea';
-var fileCount = 513;
+var fileCount = 512; // Number of blob & blob executabless
 
 exports.walk = function(test) {
   test.expect(516);
 
   git.repo('../.git', function(error, repo) {
-      repo.commit(sha, function(error, commit) {
-        var entryCount = 0;
-        commit.tree(function(error, tree) {
-          tree.walk().on('entry', function(error, index, entry) {
-              test.equals(error, null, 'There should be no error');
-              entryCount++;
-          }).on('end', function(error, entries) {
+    repo.commit(sha, function(error, commit) {
+      var entryCount = 0;
+      commit.tree(function(error, tree) {
+        tree.walk().on('entry', function(error, index, entry) {
             test.equals(error, null, 'There should be no error');
-            test.equals(entryCount, fileCount, 'The manual tree entry count and the "end" tree entry count do not match');
-            test.equals(entries.length, fileCount, 'The end entries count and the manual entry count do not match');
-            test.done();
-          });
+            entryCount++;
+        }).on('end', function(error, entries) {
+          test.equals(error, null, 'There should be no error');
+          test.equals(entryCount, fileCount, 'The manual tree entry count and the "end" tree entry count do not match');
+          test.equals(entries.length, fileCount, 'The end entries count and the manual entry count do not match');
+          test.done();
         });
       });
-   });
+    });
+  });
 };
