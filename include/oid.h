e59b4b8e80222d394b34b32bb098652539a48179
Michael Robinson <mike@pagesofinterest.net>
2013-04-03 22:07:18 +1300

    Removed Oid unused functions

--- include/oid.h
+++ include/oid.h
@@ -10,6 +10,7 @@
 
 #include <v8.h>
 #include <node.h>
+#include <string>
 
 #include "git2.h"
 
@@ -22,35 +23,34 @@ class GitOid : public ObjectWrap {
     static Persistent<Function> constructor_template;
 
     static void Initialize (Handle<v8::Object> target);
-    Handle<Value> WrapObj(Local<Object> obj);
     git_oid GetValue();
     void SetValue(git_oid oid);
 
-    int Mkstr(const char* str);
-    void Mkraw(const unsigned char* raw);
-    void Fmt(char* buffer);
-    void PathFmt(char *str);
-    char* AllocFmt();
-    char* ToString(char* buffer, size_t bufferSize);
-    void Cpy(git_oid* out);
-    int Cmp(const git_oid* a, const git_oid* b);
-
+  protected:
     GitOid() {}
     ~GitOid() {}
 
-  protected:
     static Handle<Value> New(const Arguments& args);
-    static Handle<Value> Mkstr(const Arguments& args);
-    static Handle<Value> Mkraw(const Arguments& args);
-    static Handle<Value> Fmt(const Arguments& args);
-    static Handle<Value> PathFmt(const Arguments& args);
-    static Handle<Value> AllocFmt(const Arguments& args);
-    static Handle<Value> ToString(const Arguments& args);
-    static Handle<Value> Cpy(const Arguments& args);
-    static Handle<Value> Cmp(const Arguments& args);
+
+    static Handle<Value> Sha(const Arguments& args);
+
+    static Handle<Value> FromString(const Arguments& args);
+    static void FromStringWork(uv_work_t* req);
+    static void FromStringAfterWork(uv_work_t* req);
 
   private:
     git_oid oid;
+
+    struct FromStringBaton {
+      uv_work_t request;
+      const git_error* error;
+
+      std::string fromString;
+      GitOid* oid;
+      git_oid rawOid;
+
+      Persistent<Function> callback;
+    };
 };
 
 #endif
