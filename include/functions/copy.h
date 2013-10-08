694adc5369687c47e02642941906cfc5cb21e6c2
Andreas Lind Petersen <andreas@one.com>
2013-10-08 22:15:46 +0200

    Duplicate git_error before passing it on as it will cross a thread boundary.

--- include/functions/copy.h
+++ include/functions/copy.h
@@ -1,10 +1,13 @@
 #include <v8.h>
 
+#include <string.h>
+
 #include "git2.h"
 
 #ifndef COPY_FUNCTIONS
 #define COPY_FUNCTIONS
 
+const git_error *git_error_dup(const git_error *arg);
 const git_oid *git_oid_dup(const git_oid *arg);
 const git_index_entry *git_index_entry_dup(const git_index_entry *arg);
 const git_index_time *git_index_time_dup(const git_index_time *arg);
--- src/functions/copy.cc
+++ src/functions/copy.cc
@@ -1,6 +1,14 @@
 #include <string>
+#include <cstring>
 
 #include "git2.h"
+
+const git_error *git_error_dup(const git_error *arg) {
+  git_error *result = (git_error *)malloc(sizeof(git_error));
+  result->klass = arg->klass;
+  result->message = strdup(arg->message);
+  return result;
+}
 
 const git_oid *git_oid_dup(const git_oid *arg) {
   git_oid *result = (git_oid *)malloc(sizeof(git_oid));
