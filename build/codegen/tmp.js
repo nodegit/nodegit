135104e287a8c5207127e4c42ca6c4903a08dca2
Tim Branyen <tim@tabdeveloper.com>
2014-05-08 00:12:23 -0400

    More work.

--- build/codegen/tmp.js
+++ build/codegen/tmp.js
@@ -1,27 +1,45 @@
-n = require("./new.json")
+//n = require("./new.json")
+//
+//lookup = {}
+//
+//n.forEach(function(a) {
+//  a.functions = a.functions || []
+//  a.functions.forEach(function(b) {
+//    lookup[b.cFunctionName] = {
+//      js: b.jsFunctionName,
+//      cpp: b.cppFunctionName
+//    }
+//
+//    b.args.forEach(function(c) {
+//      lookup[c.cType] = {
+//        js: c.jsClassName,
+//        cpp: c.cppClassName
+//      };
+//    })
+//
+//    lookup[b.return.cType] = {
+//      js: b.return.cppClassName,
+//      cpp: b.return.jsClassName
+//    }
+//  })
+//})
+//
+//console.log(JSON.stringify(lookup, null, 2))
+//
 
-lookup = {}
+types = require("./types.json");
+fs = require("fs");
 
-n.forEach(function(a) {
-  a.functions = a.functions || []
-  a.functions.forEach(function(b) {
-    lookup[b.cFunctionName] = {
-      js: b.jsFunctionName,
-      cpp: b.cppFunctionName
+Object.keys(types).forEach(function(typeName) {
+  var type = types[typeName];
+
+  if (!type.cpp) { return; }
+
+  if (type.cpp.indexOf("Git") === -1) {
+    if (type.cpp[0] === type.cpp[0].toUpperCase()) {
+      type.cpp = "Git" + type.cpp;
     }
+  }
+});
 
-    b.args.forEach(function(c) {
-      lookup[c.cType] = {
-        js: c.jsClassName,
-        cpp: c.cppClassName
-      };
-    })
-
-    lookup[b.return.cType] = {
-      js: b.return.cppClassName,
-      cpp: b.return.jsClassName
-    }
-  })
-})
-
-console.log(JSON.stringify(lookup, null, 2))
+fs.writeFileSync("./types.json", JSON.stringify(types, null, 2));
