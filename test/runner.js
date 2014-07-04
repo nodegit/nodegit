63e3b1f8fced28d7a15a0f588ef3ae1127bd1daa
Tim Branyen <tim@tabdeveloper.com>
2014-07-04 13:09:50 -0400

    Fix issues with fetching the fixture data

--- test/runner.js
+++ test/runner.js
@@ -1,11 +1,18 @@
 var promisify = require("promisify-node");
 var fs = promisify("fs");
-var exec = promisify("child_process").exec; 
+
+// Have to wrap exec, since it has a weird callback signature.
+var exec = promisify(function(command, callback) {
+  return require("child_process").exec(command, callback);
+});
 
 before(function(done) {
-  var url = "https://github.com/nodegit/nodegit";
+  this.timeout(5000);
 
-  fs.exists("test/repos").then(null, function() {
+  var url = "https://github.com/nodegit/nodegit";
+  var done = done.bind(null, null);
+
+  fs.exists("test/repos").then(function() {
     return fs.mkdir("test/repos").then(function() {
       return exec("git init test/repos/empty");
     }).then(function() {
@@ -17,5 +24,5 @@ before(function(done) {
         return fs.writeFile(nonrepo + "/file.txt", "This is a bogus file");
       });
     })
-  }).then(done.bind(null, undefined), done.bind(null, undefined));
+  }).then(done, done);
 });
