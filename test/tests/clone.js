c00f7d16e30f5814f537f78ba51751c435c59986
Tim Branyen <tim@tabdeveloper.com>
2014-07-13 15:52:57 -0400

    Skip network related tests

--- test/tests/clone.js
+++ test/tests/clone.js
@@ -8,6 +8,7 @@ describe("Clone", function() {
   var http = path.resolve("test/repos/http");
   var https = path.resolve("test/repos/https");
   var ssh = path.resolve("test/repos/ssh");
+  var file = path.resolve("test/repos/file");
 
   var Repository = require("../../lib/repository");
   var Clone = require("../../lib/clone");
@@ -16,11 +17,12 @@ describe("Clone", function() {
     return Promise.all([
       rimraf(http),
       rimraf(https),
-      rimraf(ssh)
+      rimraf(ssh),
+      rimraf(file),
     ]);
   });
 
-  it("can clone with http", function() {
+  it.skip("can clone with http", function() {
     var url = "http://github.com/nodegit/test.git";
 
     return Clone.clone(url, http, null).then(function(repository) {
@@ -28,7 +30,7 @@ describe("Clone", function() {
     });
   });
 
-  it("can clone with https", function() {
+  it.skip("can clone with https", function() {
     var url = "https://github.com/nodegit/test.git";
 
     return Clone.clone(url, https, null).then(function(repository) {
@@ -40,6 +42,14 @@ describe("Clone", function() {
     var url = "git@github.com:nodegit/test.git";
 
     return Clone.clone(url, ssh, null).then(function(repository) {
+      assert.ok(repository instanceof Repository);
+    });
+  });
+
+  it("can clone with filesystem", function() {
+    var url = "file://" + path.resolve("test/repos/empty");
+
+    return Clone.clone(url, file, null).then(function(repository) {
       assert.ok(repository instanceof Repository);
     });
   });
