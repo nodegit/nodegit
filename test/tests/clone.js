424c18dd352e69f6db16fa3f7429c61eafb79ce0
John Haley <johnh@axosoft.com>
2014-10-08 12:27:18 -0700

    Starting work on CloneOptions

--- test/tests/clone.js
+++ test/tests/clone.js
@@ -12,6 +12,7 @@ describe("Clone", function() {
 
   var Repository = require("../../lib/repository");
   var Clone = require("../../lib/clone");
+  var CloneOptions = require("../../lib/clone_options");
 
   before(function() {
     return Promise.all([
@@ -24,8 +25,9 @@ describe("Clone", function() {
 
   it("can clone with http", function() {
     var url = "http://github.com/nodegit/test.git";
+    var opts = CloneOptions.fromObj({ ignoreCertErrors: 1});
 
-    return Clone.clone(url, http, null).then(function(repository) {
+    return Clone.clone(url, http, opts).then(function(repository) {
       assert.ok(repository instanceof Repository);
     });
   });
