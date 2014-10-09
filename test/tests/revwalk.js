58372b1d06d478719f7a195727559f6736b6108c
Steve Smith <steve@orderedlist.com>
2014-10-09 15:09:02 -0400

    Add revwalk.next and revwalk.simplifyFirstParent

---
+++ test/tests/revwalk.js
@@ -0,0 +1,65 @@
+var assert = require("assert");
+var path = require("path");
+
+describe("Revwalk", function() {
+  var reposPath = path.resolve("test/repos/workdir/.git");
+  //var newRepo = path.resolve("test/repos/newrepo");
+
+  var Repository = require("../../lib/repository");
+  var Revwalk = require("../../lib/revwalk");
+  var Oid = require("../../lib/oid");
+  //var Index = require("../../lib/index");
+
+  before(function() {
+    var test = this;
+
+    return Repository.open(reposPath).then(function(repository) {
+      test.repository = repository;
+      test.walker = repository.createRevWalk();
+    });
+  });
+
+  it("can create a walker", function() {
+    assert.ok(this.walker instanceof Revwalk);
+  });
+
+  it("can push an object", function() {
+    var test = this;
+
+    return this.repository.getMaster().then(function(master) {
+      test.walker.push(master.id());
+      return test.walker.next().then(function(commit) {
+        assert.equal(master.sha(), commit.toString());
+      });
+    });
+  });
+
+  it("can hide an object", function() {
+    var test = this;
+
+    return this.repository.getMaster().then(function(master) {
+      test.walker.push(master.id());
+      test.walker.hide(Oid.fromstr("a03e044fcb45c654d4e15a4e495a6a0c6e632854"));
+      return test.walker.next().then(function(commit) {
+        return test.walker.next().then(function(commit) {
+          assert.equal(commit.toString(),
+            "32789a79e71fbc9e04d3eff7425e1771eb595150");
+        });
+      });
+    });
+  });
+
+  it("can simplify to first parent", function() {
+    var test = this;
+
+    return this.repository.getMaster().then(function(master) {
+      test.walker.simplifyFirstParent();
+      return test.walker.next().then(function(commit) {
+        return test.walker.next().then(function(commit) {
+          assert.equal(commit.toString(),
+            "b8a94aefb22d0534cc0e5acf533989c13d8725dc");
+        });
+      });
+    });
+  });
+});
