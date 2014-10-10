85ef130c2440de7258edaaeaea3285d864f82004
John Haley <johnh@axosoft.com>
2014-10-10 11:29:11 -0700

    Starting work on getting structs to be wrapped properly

---
+++ generate/templates/struct_header.h
@@ -0,0 +1,44 @@
+#ifndef {{ cppClassName|upper }}_H
+#define {{ cppClassName|upper }}_H
+
+#include <nan.h>
+#include <string>
+
+extern "C" {
+#include <git2.h>
+}
+
+{%each dependencies as dependency%}
+#include "{{ dependency }}"
+{%endeach%}
+
+using namespace node;
+using namespace v8;
+
+class {{ cppClassName }} : public ObjectWrap {
+  public:
+
+    static Persistent<Function> constructor_template;
+    static void Initialize (Handle<v8::Object> target);
+
+    {{ cType }} *GetValue();
+
+    static Handle<Value> New(void *raw);
+
+  private:
+    {{ cppClassName }}({{ cType }} *raw);
+    ~{{ cppClassName }}();
+
+    static NAN_METHOD(New);
+
+    {%each fields as field%}
+      {%if not field.ignore%}
+    static NAN_GETTER(Get{{ field.cppFunctionName }});
+    static NAN_SETTER(Set{{ field.cppFunctionName }});
+      {%endif%}
+    {%endeach%}
+
+    {{ cType }} *raw;
+};
+
+#endif
