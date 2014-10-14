222432cd01b14a29333b558f347956171df3eba5
Tim Branyen <tim@tabdeveloper.com>
2014-10-14 18:23:19 -0400

    Clean up more templates

--- generate/partials/convert_from_v8.cc
+++ generate/partials/convert_from_v8.cc
@@ -16,7 +16,7 @@
     {%--
       // FIXME: should recursively call convertFromv8.
     --%}
-        from_{{ name }}[i] = ObjectWrap::Unwrap<{{ arrayElementCppClassName }}>(tmp_{{ name }}->Get(NanNew<Number>(static_cast<double>(i)))->ToObject())->GetValue();
+      from_{{ name }}[i] = ObjectWrap::Unwrap<{{ arrayElementCppClassName }}>(tmp_{{ name }}->Get(NanNew<Number>(static_cast<double>(i)))->ToObject())->GetValue();
       }
   {%elsif cppClassName == 'Function' %}
   {%elsif cppClassName == 'Buffer' %}
@@ -26,14 +26,14 @@
 
   from_{{ name }} = ({{ cType }}) {{ additionalCast }} {{ cast }} args[{{ jsArg }}]->To{{ cppClassName }}()->Value();
   {%else%}
-
-  from_{{ name }} = ObjectWrap::Unwrap<{{ cppClassName }}>(args[{{ jsArg }}]->ToObject())->GetValue();
-
+    from_{{ name }} = ObjectWrap::Unwrap<{{ cppClassName }}>(args[{{ jsArg }}]->ToObject())->GetValue();
   {%endif%}
 
   {%if isOptional %}
-    } else {
-      from_{{ name }} = 0;
-    }
+  }
+  else {
+    from_{{ name }} = 0;
+  }
+
   {%endif%}
 {%endif%}
