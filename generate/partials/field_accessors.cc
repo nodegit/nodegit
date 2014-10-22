dc7f31e76293f3496fc4104cda04dcf050cf4135
Tim Branyen <tim@tabdeveloper.com>
2014-10-22 02:43:28 -0400

    Getfunction

--- generate/partials/field_accessors.cc
+++ generate/partials/field_accessors.cc
@@ -7,7 +7,7 @@ NAN_GETTER({{ cppClassName }}::Get{{ field.cppFunctionName }}) {
   {{ cppClassName }} *wrapper = ObjectWrap::Unwrap<{{ cppClassName }}>(args.This());
 
   {%if field.hasConstructor | or field.isFunction | or field.payloadFor %}
-  NanReturnValue(wrapper->{{ field.name }});
+  NanReturnValue(wrapper->{{ field.name }}->GetFunction());
   {%elsif field.cppClassName == 'String' %}
   if (wrapper->GetValue()->{{ field.name }}) {
     NanReturnValue(NanNew<String>(wrapper->GetValue()->{{ field.name }}));
