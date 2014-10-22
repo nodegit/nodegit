aa006c3ab48305e401f8eb8e7205171b879ea6d7
Tim Branyen <tim@tabdeveloper.com>
2014-10-21 21:23:54 -0400

    I've made a mess of things

--- generate/partials/field_accessors.cc
+++ generate/partials/field_accessors.cc
@@ -94,7 +94,9 @@ void {{ cppClassName }}::{{ field.name }}_asyncAfter(uv_work_t* req, int status)
   {{ field.name|titleCase }}Baton* baton = static_cast<{{ field.name|titleCase }}Baton*>(req->data);
   {{ cppClassName }}* instance = static_cast<{{ cppClassName }}*>(baton->payload);
 
-  if (!instance->{{ field.name }}->IsFunction()) {
+  Local<Value> local_{{ field.name }} = NanNew(instance->{{ field.name }});
+
+  if (!local_{{ field.name }}->IsFunction()) {
     {%if field.returnType == "int" %}
     baton->result = {{ field.returnNoResults }}; // no results acquired
     {%endif%}
@@ -106,17 +108,15 @@ void {{ cppClassName }}::{{ field.name }}_asyncAfter(uv_work_t* req, int status)
     {%each field.args|argsInfo as arg %}
       {%if arg.name == "payload" %}
       {%-- payload is always the last arg --%}
-    NanNew<Value>(instance->{{ fields|payloadFor field.name }});
-    Local<Value>::New()
+    NanNew(instance->{{ fields|payloadFor field.name }}),
       {%elsif arg.isJsArg %}
-    NanNew<Value>(baton->{{ arg.name }}),
-
+    NanNew(baton->{{ arg.name }}),
       {%endif%}
     {%endeach%}
   };
 
   TryCatch tryCatch;
-  Local<Value> result = Local<Value>::New(Function::Cast(*instance->{{ field.name }})->Call(Context::GetCurrent()->Global(), {{ field.args|jsArgsCount }}, argv));
+  Local<Value> result = NanNew<Function>(instance->{{ field.name }}.As<Function>())->Call(NanGetCurrentContext()->Global(), {{ field.args|jsArgsCount }}, argv);
   {{ field.returnType }} resultStatus;
 
   {%each field|returnsInfo true false as _return%}
