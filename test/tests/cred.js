435924b16a354fb7c19816b09a6802a074daf843
John Haley <johnh@axosoft.com>
2014-10-23 15:17:25 -0700

    Added a few unit tests for credentials
    These just confirm that the credentials are actually created not
    that we can do anything with them right now.

---
+++ test/tests/cred.js
@@ -0,0 +1,28 @@
+var assert = require("assert");
+
+describe("Cred", function() {
+  var NodeGit = require("../../");
+
+  it("can create default credentials", function() {
+    var defaultCreds = NodeGit.Cred.defaultNew();
+
+    assert(defaultCreds instanceof NodeGit.Cred);
+  });
+
+  it("can create ssh credentials using passed keys", function() {
+    var sshCreds
+      = NodeGit.Cred.sshKeyNew(
+        "username",
+        "public key",
+        "private key",
+        "passphrase");
+
+    assert(sshCreds instanceof NodeGit.Cred);
+  });
+
+  it("can create credentials using plaintext", function() {
+    var plaintextCreds = NodeGit.Cred.userpassPlaintextNew("username", "password");
+
+    assert(plaintextCreds instanceof NodeGit.Cred);
+  });
+});
