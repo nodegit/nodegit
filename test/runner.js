eadcc0abb96f2687b3c4cfd0067689a5ae572eea
Max Korp <Maxkorp@8bytealchemy.com>
2014-10-27 10:13:48 -0700

    point tests to use test repo now

--- test/runner.js
+++ test/runner.js
@@ -9,7 +9,7 @@ var exec = promisify(function(command, callback) {
 before(function(done) {
   this.timeout(150000);
 
-  var url = "https://github.com/nodegit/nodegit";
+  var url = "https://github.com/nodegit/test";
   var done = done.bind(null, null);
 
   fs.exists("test/repos").then(function() {
