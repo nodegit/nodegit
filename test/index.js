c4aa95892a8cbe657d3bc4f3d137715ce4d8990e
John Haley <johnh@axosoft.com>
2014-11-24 16:31:06 -0700

    Fixed tests running on windows

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
-var args = "cover _mocha -- runner tests --report=lcov".split(" ");
+var args = [
+    "cover",
+    process.platform != "win32" ? "_mocha" : "../node_modules/mocha/bin/_mocha",
+    "--",
+    "runner",
+    "tests",
+    "--report=lcov",
+    "--expose-gc"
+  ];
 
 require("child_process").fork("../node_modules/istanbul/lib/cli.js", args, {
-  cwd: __dirname,
-  execArgv: [
-    process.platform !== "win32" ? "--expose-gc" : ""
-  ]
+  cwd: __dirname
 });
