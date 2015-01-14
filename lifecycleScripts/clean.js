5764d4c38929742a69462344c7c34116b0b8cde5
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-01-14 11:51:50 -0700

    overhaul the rest of the build process includes cleaning of npm registry based installs and a new debug build

---
+++ lifecycleScripts/clean.js
@@ -0,0 +1,138 @@
+var fse = require("fs-extra");
+var path = require("path");
+var cp = require("child_process");
+var npm = require("npm");
+var Promise = require("nodegit-promise");
+
+var rooted = path.join.bind(path, __dirname, "..");
+if (!fse.existsSync(rooted(".didntcomefromthenpmregistry"))) {
+  return;
+}
+
+npm.load({
+  "production": true
+}, function() {
+
+  var filesToDelete = [
+    "build/Makefile",
+    "build/binding.Makefile",
+    "build/config.gypi",
+    "build/gyp-mac-tool",
+    "build/nodegit.target.mk",
+    "build/vendor",
+
+    "build/Release/git2.a",
+    "build/Release/http_parser.a",
+    "build/Release/linker.lock",
+    "build/Release/obj.target",
+    "build/Release/openssl.a",
+    "build/Release/ssh2.a",
+    "build/Release/zlib.a",
+
+    "build/Debug/git2.a",
+    "build/Debug/http_parser.a",
+    "build/Debug/linker.lock",
+    "build/Debug/obj.target",
+    "build/Debug/openssl.a",
+    "build/Debug/ssh2.a",
+    "build/Debug/zlib.a",
+
+    "example",
+    "generate",
+    "include",
+    "lifecycleScripts",
+    "src",
+    "test",
+    "vendor",
+
+    ".astylerc",
+    ".editorconfig",
+    ".gitingore",
+    ".gitmodules",
+    ".jshintrc",
+    ".npmignore",
+    ".travis.yml",
+    "appveyor.yml",
+    "binding.gyp",
+    "TESTING.md"
+  ];
+
+  var modulesToPrune = [
+    "find-parent-dir",
+    "for-node-webkit",
+    "fs-extra",
+    "node-pre-gyp",
+    "npm"
+  ];
+
+  console.info("[nodegit] Cleaning up after ourselves.");
+
+  Promise.all([
+    cleanFiles(),
+    pruneDevDependencies(),
+    pruneOtherDependencies()
+  ]).then(
+    function success() {
+      console.info("[nodegit] Done cleaning, enjoy!");
+    },
+    function failure() {
+      console.error("[nodegit] Couldn't finish cleaning.");
+    }
+  );
+
+  function cleanFiles() {
+    console.info("[nodegit] Deleting unneeded files.");
+    return new Promise(function(resolve, reject) {
+      var failures = filesToDelete.some(function(deletable) {
+        try {
+          fse.removeSync(rooted(deletable));
+          return false
+        }
+        catch (e) {
+          console.info("[nodegit] Error deleting files. Errored on " + rooted(deletable)) + ".";
+          console.error(e);
+
+          reject(e);
+          return true;
+        }
+      });
+
+      if (!failures) {
+        console.info("[nodegit] Finished deleting files.");
+        resolve();
+      }
+    });
+  }
+
+  function pruneDevDependencies() {
+    console.info("[nodegit] Pruning devdependencies.");
+    return new Promise(function (resolve, reject) {
+      npm.commands.prune(function(err) {
+        if (err) {
+          console.error("[nodegit] Failed pruning devdependencies.");
+          reject(err);
+        }
+        else {
+          console.info("[nodegit] Successfully pruned devdependencies.");
+          resolve();
+        }
+      });
+    });
+  }
+
+  function pruneOtherDependencies() {
+    console.info("[nodegit] Cleaning unneeded dependencies.");
+    return new Promise(function (resolve, reject) {
+      npm.commands.uninstall(modulesToPrune, function(err) {
+        if (err) {
+          console.drror("[nodegit] Failed cleaning unneeded dependencies.");
+          reject(err);
+        }
+        else {
+          console.info("[nodegit] Successfully cleaned unneeded dependencies.");
+          resolve();
+        }
+      });
+    });
+  }
+});
---
+++ lifecycleScripts/install.js
@@ -0,0 +1,104 @@
+var Promise = require("nodegit-promise");
+var promisify = require("promisify-node");
+var path = require("path");
+var fs = require("fs");
+
+var local = path.join.bind(path, __dirname);
+var pkg = require(local("../package"));
+var cp = require("child_process");
+
+var checkPrepared = require(local("checkPrepared"));
+var forNodeWebkit = require("for-node-webkit");
+var prepareForBuild = require(local("prepareForBuild"));
+
+var exec = promisify(function(command, opts, callback) {
+  return require("child_process").exec(command, opts, callback);
+});
+
+
+forNodeWebkit(local(".."))
+  .then(function(results) {
+    return results.nwVersion;
+  })
+  .then(function(nodeWebkitVersion) {
+    if (nodeWebkitVersion) {
+      console.info("[nodegit] Must build for node-webkit");
+      return checkAndBuild(nodeWebkitVersion);
+    }
+    if (!fs.existsSync(local("../.didntcomefromthenpmregistry"))) {
+      checkAndBuild();
+    }
+    if (process.env.BUILD_DEBUG) {
+      console.info("[nodegit] Doing a debug build, no fetching allowed.");
+      return checkAndBuild();
+    }
+    if (process.env.BUILD_ONLY) {
+      console.info("[nodegit] BUILD_ONLY is set to true, no fetching allowed.");
+      return checkAndBuild();
+    }
+    console.info("[nodegit] Fetching binary from S3.");
+    return exec("node-pre-gyp build")
+      .then(
+        function() {
+          console.info("[nodegit] Completed installation successfully.");
+          return Promise.done();
+        },
+        function() {
+          console.info("[nodegit] Failed to install prebuilt binary, building manually.");
+          return checkAndBuild();
+        }
+      );
+  })
+
+
+function checkAndBuild(nwVersion) {
+  console.info("[nodegit] Making sure dependencies are available and native code is generated");
+
+  return checkPrepared.checkAll()
+    .then(function(allGood) {
+      if (allGood) {
+        return build(nwVersion);
+      }
+      else {
+        console.info("[nodegit] Something is missing, retrieving dependencies and regenerating code");
+        return prepareForBuild()
+          .then(function() {
+            return build(nwVersion);
+          });
+      }
+    });
+}
+
+function build(nwVersion) {
+  console.info("[nodegit] Everything is ready to go, attempting compilation");
+  if (nwVersion) {
+    console.info("[nodegit] Building native node-webkit module.");
+  }
+  else {
+    console.info("[nodegit] Building native node module.");
+  }
+
+  var opts = {
+    cwd: local(".."),
+    maxBuffer: Number.MAX_VALUE
+  };
+
+  var builder = nwVersion ? "nw-gyp" : "node-gyp";
+  var target = (nwVersion ? "--target=\"" + nwVersion + "\"": "");
+  var debug = (process.env.BUILD_DEBUG ? " --debug" : "");
+  var cmd = path.resolve(".", "node_modules", ".bin", builder)
+    + " clean configure"
+    + debug
+    + target
+    + " build";
+
+  return exec(cmd, opts)
+    .then(function() {
+      console.info("[nodegit] Compilation complete.");
+      console.info("[nodegit] Completed installation successfully.");
+    },
+    function(err, stderr) {
+      console.error(err);
+      console.error(stderr);
+    });
+}
