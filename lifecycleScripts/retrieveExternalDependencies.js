0506f4d619026c9410f4eb9e4ef28986716f0707
billt2006 <billt2006@gmail.com>
2015-02-03 01:47:28 -0500

    fix building when a space is in the path

--- lifecycleScripts/retrieveExternalDependencies.js
+++ lifecycleScripts/retrieveExternalDependencies.js
@@ -78,7 +78,7 @@ function getVendorLib(name) {
             if ((name == "libssh2") && (process.platform !== "win32")) {
               return new Promise(function(resolve, reject) {
                 console.info("[nodegit] Configuring libssh2.");
-                cp.exec(
+                cp.execFile(
                   rooted(vendorPath) + "configure",
                   {cwd: rooted(vendorPath)},
                   function(err, stdout, stderr) {
