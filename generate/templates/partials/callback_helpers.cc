c7b1db5d52c39e1df34633df867784ee78f0b7d7
John Haley <johnh@axosoft.com>
2015-02-10 09:50:07 -0700

    Updated some dependencies

--- generate/templates/partials/callback_helpers.cc
+++ generate/templates/partials/callback_helpers.cc
@@ -82,6 +82,7 @@ void {{ cppClassName }}::{{ cppFunctionName }}_{{ cbFunction.name }}_asyncAfter(
 
   {{ cbFunction.return.type }} resultStatus;
 
+  {}
   {% each cbFunction|returnsInfo true false as _return %}
     if (result.IsEmpty() || result->IsNativeError()) {
       baton->result = {{ cbFunction.return.error }};
