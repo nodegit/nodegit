3f0eaee8d6fa77e2fce9086c56bba8acbecfbb8c
John Haley <johnh@axosoft.com>
2015-02-12 13:25:51 -0700

    Revert "Fixed returns for async functions when there is only 1 return"
    This reverts commit 902b837814b71f4105924d338ae523dadfe9802f.

--- generate/templates/partials/async_function.cc
+++ generate/templates/partials/async_function.cc
@@ -90,13 +90,14 @@ void {{ cppClassName }}::{{ cppFunctionName }}Worker::HandleOKCallback() {
     Handle<v8::Value> to;
       {%if .|returnsCount > 1 %}
     Handle<Object> result = NanNew<Object>();
-        {%each .|returnsInfo 0 1 as _return %}
-          {%partial convertToV8 _return %}
-          {%if .|returnsCount > 1 %}
+      {%endif%}
+      {%each .|returnsInfo 0 1 as _return %}
+        {%partial convertToV8 _return %}
+        {%if .|returnsCount > 1 %}
     result->Set(NanNew<String>("{{ _return.returnNameOrName }}"), to);
-          {%endif%}
-        {%endeach%}
-      {%elsif .|returnsCount == 1 %}
+        {%endif%}
+      {%endeach%}
+      {%if .|returnsCount == 1 %}
     Handle<v8::Value> result = to;
       {%endif%}
     {%endif%}
