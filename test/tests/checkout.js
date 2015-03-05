b09fa0269eeec02cad3ba5ef4f457641915fcace
Steve Smith <steve@orderedlist.com>
2015-03-05 09:42:51 -0500

    Add test for struct strarray in checkout

--- test/tests/checkout.js
+++ test/tests/checkout.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var path = require("path");
+var fse = require("fs-extra");
 var local = path.join.bind(path, __dirname);
 
 describe("Checkout", function() {
@@ -7,8 +8,12 @@ describe("Checkout", function() {
   var Repository = NodeGit.Repository;
   var Checkout = NodeGit.Checkout;
 
+  var readMeName = "README.md";
+  var packageJsonName = "package.json";
   var packageJsonOid = "0fa56e90e096a4c24c785206b826ab914ea3de1e";
   var reposPath = local("../repos/workdir");
+  var readMePath = local("../repos/workdir/" + readMeName);
+  var packageJsonPath = local("../repos/workdir/" + packageJsonName);
 
   beforeEach(function() {
     var test = this;
@@ -30,6 +35,42 @@ describe("Checkout", function() {
       var packageJson = blob.toString();
 
       assert.ok(~packageJson.indexOf("\"ejs\": \"~1.0.0\","));
+    });
+  });
+
+  it("can force checkout a single file", function() {
+    var test = this;
+
+    var packageContent = fse.readFileSync(packageJsonPath, "utf-8");
+    var readmeContent = fse.readFileSync(readMePath, "utf-8");
+
+    assert.notEqual(packageContent, "");
+    assert.notEqual(readmeContent, "");
+
+    fse.outputFileSync(readMePath, "");
+    fse.outputFileSync(packageJsonPath, "");
+
+    var opts = {
+      checkoutStrategy: Checkout.STRATEGY.FORCE,
+      paths: packageJsonName
+    };
+
+    return Checkout.head(test.repository, opts)
+    .then(function() {
+      var resetPackageContent = fse.readFileSync(packageJsonPath, "utf-8");
+      var resetReadmeContent = fse.readFileSync(readMePath, "utf-8");
+
+      assert.equal(resetPackageContent, packageContent);
+      assert.equal(resetReadmeContent, "");
+
+      var resetOpts = {
+        checkoutStrategy: Checkout.STRATEGY.FORCE
+      };
+
+      return Checkout.head(test.repository, resetOpts);
+    }).then(function() {
+      var resetContent = fse.readFileSync(readMePath, "utf-8");
+      assert.equal(resetContent, readmeContent);
     });
   });
 
