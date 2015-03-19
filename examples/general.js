f3d0812adaef20a9162cd229fe315449df22e3fb
Bill Thornton <bill@exclamationlabs.com>
2015-03-19 12:51:54 -0400

    Fix symbolic reference handling in getReferences

--- examples/general.js
+++ examples/general.js
@@ -343,23 +343,18 @@ nodegit.Repository.open(path.resolve(__dirname, "../.git"))
     // references such as branches, tags and remote references (everything in
     // the .git/refs directory).
 
-    return repo.getReferenceNames(nodegit.Reference.TYPE.ALL);
+    return nodegit.Repository.getReferences(repo,
+      nodegit.Reference.TYPE.LISTALL, false, true);
   })
 
-  .then(function(referenceNames) {
-    var promises = [];
-
-    referenceNames.forEach(function(referenceName) {
-      promises.push(repo.getReference(referenceName).then(function(reference) {
-        if (reference.isConcrete()) {
-          console.log("Reference:", referenceName, reference.target());
-        } else if (reference.isSymbolic()) {
-          console.log("Reference:", referenceName, reference.symbolicTarget());
-        }
-      }));
+  .then(function(references) {
+    references.forEach(function(reference) {
+      if (reference.isConcrete()) {
+        console.log("Reference:", reference.name(), reference.target());
+      } else if (reference.isSymbolic()) {
+        console.log("Reference:", reference.name(), reference.symbolicTarget());
+      }
     });
-
-    return Promise.all(promises);
   })
 
   .done(function() {
