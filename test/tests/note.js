fab9237a544a01c1a34b99c0cf82132e3bbf2ab1
Tim <tim@tabdeveloper.com>
2015-03-22 01:41:44 -0400

    Enable note foreach and remove async
    And added testing goodness!

--- test/tests/note.js
+++ test/tests/note.js
@@ -6,7 +6,7 @@ describe("Note", function() {
   var NodeGit = require("../../");
   var Note = NodeGit.Note;
   var Signature = NodeGit.Signature;
-  var reposPath = local("../repos/merge");
+  var reposPath = local("../../");
 
   beforeEach(function() {
     var test = this;
@@ -37,16 +37,33 @@ describe("Note", function() {
     });
   });
 
+  it("can iterate all notes", function() {
+    var test = this;
+    var noteRef = "refs/notes/commits";
+    var ref = null;
+
+    return Note.foreach(this.repository, noteRef, function(blobId, objectId) {
+      ref = objectId;
+    }).then(function() {
+      return NodeGit.Note.read(test.repository, noteRef, ref)
+        .then(function(note) {
+          assert.equal(note.message(), "Testing!");
+        });
+    });
+  });
+
   it("can be removed", function(done) {
+    var test = this;
     var sha = this.commit.id();
     var noteRef = "refs/notes/commits";
     var sig = Signature.create("John", "john@doe.com", Date.now(), 0);
 
-    Note.remove(this.repository, noteRef, sig, sig, sha);
-
-    return Note.read(this.repository, noteRef, sha).catch(function(ex) {
-      assert.equal(ex.message, "Note could not be found");
-      done();
-    });
+    return Note.remove(this.repository, noteRef, sig, sig, sha)
+      .then(function() {
+        return Note.read(test.repository, noteRef, sha).catch(function(ex) {
+          assert.equal(ex.message, "Note could not be found");
+          done();
+        });
+      });
   });
 });
