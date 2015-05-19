9e790942642c897783da992cd9742dd9fe84b2eb
John Haley <johnh@axosoft.com>
2015-05-19 12:51:02 -0700

    Add failing test

--- test/tests/attr.js
+++ test/tests/attr.js
@@ -19,6 +19,10 @@ describe("Attr", function() {
       });
   });
 
+  it("breaks right now", function() {
+    assert.equal(1, 0);
+  });
+
   it("can add a macro definition", function() {
     var error = Attr.addMacro(this.repository, "binary", "-diff -crlf");
 
