6318739ff4b100f44f8419a3d9537639340cd31a
vanadium23 <chernoffivan@gmail.com>
2015-05-29 09:36:54 +0300

    Make individual test for each status
    * Also add helper function to test
    * And skip for statuses CURRENT & WT_UNREADABLE

--- test/tests/status_file.js
+++ test/tests/status_file.js
@@ -7,37 +7,79 @@ describe("StatusFile", function() {
 
   var pathName = "README.md";
 
-  var checkStatusFile = function(status) {
-    it("identifies the proper statuses for " + status, function() {
-      var statusFile = new StatusFile({
-        path: pathName,
-        status: Status.STATUS[status]
-      });
-      var specialFunction = status.replace(/^(WT|INDEX)_/, "");
-      specialFunction = "is" +
-        specialFunction[0] +
-        specialFunction.substring(1).toLowerCase();
-      if (/^WT_/.test(status)) {
-        assert.ok(statusFile.inWorkingTree());
-        assert.ok(!statusFile.inIndex());
-      }
-      if (/^INDEX_/.test(status)) {
-        assert.ok(!statusFile.inWorkingTree());
-        assert.ok(statusFile.inIndex());
-      }
-      assert.equal(statusFile.path(), pathName);
-      assert.equal(statusFile.statusBit(), Status.STATUS[status]);
-      assert.equal(statusFile.status(), status);
-      assert.ok(statusFile[specialFunction]());
+  function testStatusFile(status) {
+    var statusFile = new StatusFile({
+      path: pathName,
+      status: Status.STATUS[status]
     });
-  };
-
-  for (var status in Status.STATUS) {
-    if (status === "CURRENT" || status === "WT_UNREADABLE") {
-      continue;
+    var specialFunction = status.replace(/^(WT|INDEX)_/, "");
+    specialFunction = "is" +
+      specialFunction[0] +
+      specialFunction.substring(1).toLowerCase();
+    if (/^WT_/.test(status)) {
+      assert.ok(statusFile.inWorkingTree());
+      assert.ok(!statusFile.inIndex());
     }
-    checkStatusFile(status);
+    if (/^INDEX_/.test(status)) {
+      assert.ok(!statusFile.inWorkingTree());
+      assert.ok(statusFile.inIndex());
+    }
+    assert.equal(statusFile.path(), pathName);
+    assert.equal(statusFile.statusBit(), Status.STATUS[status]);
+    assert.equal(statusFile.status(), status);
+    assert.ok(statusFile[specialFunction]());
   }
 
+  it.skip("identifies the proper statuses for CURRENT", function() {
+    testStatusFile("CURRENT");
+  });
+
+  it.skip("identifies the proper statuses for WT_UNREADABLE", function() {
+    testStatusFile("WT_UNREADABLE");
+  });
+
+  it("identifies the proper statuses for WT_NEW", function() {
+    testStatusFile("WT_NEW");
+  });
+
+  it("identifies the proper statuses for WT_MODIFIED", function() {
+    testStatusFile("WT_MODIFIED");
+  });
+
+  it("identifies the proper statuses for WT_DELETED", function() {
+    testStatusFile("WT_DELETED");
+  });
+
+  it("identifies the proper statuses for WT_TYPECHANGE", function() {
+    testStatusFile("WT_TYPECHANGE");
+  });
+
+  it("identifies the proper statuses for WT_RENAMED", function() {
+    testStatusFile("WT_RENAMED");
+  });
+
+  it("identifies the proper statuses for IGNORED", function() {
+    testStatusFile("IGNORED");
+  });
+
+  it("identifies the proper statuses for INDEX_NEW", function() {
+    testStatusFile("INDEX_NEW");
+  });
+
+  it("identifies the proper statuses for INDEX_MODIFIED", function() {
+    testStatusFile("INDEX_MODIFIED");
+  });
+
+  it("identifies the proper statuses for INDEX_DELETED", function() {
+    testStatusFile("INDEX_DELETED");
+  });
+
+  it("identifies the proper statuses for INDEX_TYPECHANGE", function() {
+    testStatusFile("INDEX_TYPECHANGE");
+  });
+
+  it("identifies the proper statuses for INDEX_RENAMED", function() {
+    testStatusFile("INDEX_RENAMED");
+  });
 
 });
