7352ed787a7fad13e1fbd93a951827c65705fa9d
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-06-12 11:26:21 -0700

    add --no-ff and --ff-only to mergebranches

--- test/tests/merge.js
+++ test/tests/merge.js
@@ -243,6 +243,119 @@ describe("Merge", function() {
     });
   });
 
+  it("can merge --no-ff a fast-forward using the convenience method",
+    function() {
+    var ourFileName = "ourNewFile.txt";
+    var theirFileName = "theirNewFile.txt";
+
+    var ourFileContent = "I like Toll Roads. I have an EZ-Pass!";
+    var theirFileContent = "I'm skeptical about Toll Roads";
+
+    var ourSignature = NodeGit.Signature.create
+    ("Ron Paul", "RonPaul@TollRoadsRBest.info", 123456789, 60);
+    var theirSignature = NodeGit.Signature.create
+    ("Greg Abbott", "Gregggg@IllTollYourFace.us", 123456789, 60);
+
+    var repository = this.repository;
+    var ourCommit;
+    var theirCommit;
+    var ourBranch;
+    var theirBranch;
+
+    return fse.writeFile(
+      path.join(repository.workdir(), ourFileName),
+      ourFileContent)
+    // Load up the repository index and make our initial commit to HEAD
+    .then(function() {
+      return repository.openIndex()
+      .then(function(index) {
+        index.read(1);
+        index.addByPath(ourFileName);
+        index.write();
+
+        return index.writeTree();
+      });
+    })
+    .then(function(oid) {
+      assert.equal(oid.toString(),
+      "11ead82b1135b8e240fb5d61e703312fb9cc3d6a");
+
+      return repository.createCommit("HEAD", ourSignature,
+      ourSignature, "we made a commit", oid, []);
+    })
+    .then(function(commitOid) {
+      assert.equal(commitOid.toString(),
+      "91a183f87842ebb7a9b08dad8bc2473985796844");
+
+      return repository.getCommit(commitOid).then(function(commit) {
+        ourCommit = commit;
+      }).then(function() {
+        return repository.createBranch(ourBranchName, commitOid)
+        .then(function(branch) {
+          ourBranch = branch;
+          return repository.createBranch(theirBranchName, commitOid);
+        });
+      });
+    })
+    .then(function(branch) {
+      theirBranch = branch;
+      return fse.writeFile(path.join(repository.workdir(), theirFileName),
+      theirFileContent);
+    })
+    .then(function() {
+      return repository.openIndex()
+      .then(function(index) {
+        index.read(1);
+        index.addByPath(theirFileName);
+        index.write();
+
+        return index.writeTree();
+      });
+    })
+    .then(function(oid) {
+      assert.equal(oid.toString(),
+      "76631cb5a290dafe2959152626bb90f2a6d8ec94");
+
+      return repository.createCommit(theirBranch.name(), theirSignature,
+      theirSignature, "they made a commit", oid, [ourCommit]);
+    })
+    .then(function(commitOid) {
+      assert.equal(commitOid.toString(),
+      "0e9231d489b3f4303635fc4b0397830da095e7e7");
+
+      return repository.getCommit(commitOid).then(function(commit) {
+        theirCommit = commit;
+      });
+    })
+    .then(function() {
+      var opts = {checkoutStrategy: NodeGit.Checkout.STRATEGY.FORCE};
+      return repository.checkoutBranch(ourBranchName, opts);
+    })
+    .then(function() {
+      return repository.mergeBranches(
+        ourBranchName,
+        theirBranchName,
+        ourSignature,
+        true);
+    })
+    .then(function(oid) {
+      assert.equal(oid.toString(),
+      "6806d22d2b6c0095b29dc5ec51829caeb67861f1");
+
+      return repository.getBranchCommit(ourBranchName)
+        .then(function(branchCommit) {
+          assert.equal(oid.toString(), branchCommit.toString());
+        });
+    })
+    .then(function() {
+      return repository.getStatus();
+    })
+    .then(function(statuses) {
+      // make sure we didn't change the index
+      assert.equal(statuses.length, 0);
+    });
+  });
+
   it("can merge cleanly using the convenience method", function() {
     var initialFileName = "initialFile.txt";
     var ourFileName = "ourNewFile.txt";
