57a1a1bbe05475ce2f9cb0152b9dbd2a3d23a73f
joshaber <joshaber@gmail.com>
2015-11-11 22:50:09 -0500

    Let's try using electron to run the tests.

---
+++ lifecycleScripts/runTests.js
@@ -0,0 +1,32 @@
+var spawn = require("child_process").spawn;
+var Promise = require("nodegit-promise");
+
+var determineTarget = require("./determineTarget");
+
+return determineTarget()
+  .then(function(targetInfo) {
+    var opts = {
+      cwd: ".",
+      maxBuffer: Number.MAX_VALUE,
+      env: process.env
+    };
+    var binaryName = "node";
+    var args = ["--expose-gc", "test"];
+    if (targetInfo.target === "electron") {
+      binaryName = "electron";
+      opts.env.ATOM_SHELL_INTERNAL_RUN_AS_NODE = "1";
+    }
+
+    return new Promise(function(resolve, reject) {
+      var task = spawn(binaryName, args, opts);
+      task.stdout.pipe(process.stdout);
+      task.stderr.pipe(process.stderr);
+      task.on("exit", function(code) {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(code);
+        }
+      });
+    });
+  });
