1563d0d52483426a3b977ab0495747a88af83a60
joshaber <joshaber@gmail.com>
2015-12-04 16:47:57 -0500

    Fallback to source when developing or when getting a binary fails.

---
+++ lifecycleScripts/install.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var path = require("path");
+var prepareForBuild = require("./prepareForBuild");
+
+var exec = promisify(function(command, opts, callback) {
+  return require("child_process").exec(command, opts, callback);
+});
+
+function install(type) {
+  var helperPath = path.resolve(".", "node_modules", ".bin", "npg-npmrc-helper");
+  var cmd = "node " + helperPath + " install " + type;
+  return exec(cmd);
+}
+
+function buildFromSource() {
+  return prepareForBuild().then(function() {
+    return install("--build-from-source");
+  });
+}
+
+if (fs.existsSync("../.didntcomefromthenpmregistry")) {
+  // If we're developing then we always want to build from source.
+  return buildFromSource();
+} else {
+  // Don't use node-pre-gyp's built-in fallback. We need to prepareForBuild()
+  // first.
+  return install("--fallback-to-build=false").catch(buildFromSource());
+}
