5d1d732b5f6c7b164ebc5d5809e9b8300eb2eb64
Stjepan Rajko <stjepanr@axosoft.com>
2015-12-11 14:59:45 -0700

    Start dedicated thread / loop for libgit2 calls

---
+++ generate/templates/manual/include/nodegit.h
@@ -0,0 +1,8 @@
+#ifndef NODEGIT_H
+#define NODEGIT_H
+
+#include "nan.h"
+
+extern uv_loop_t *libgit2_loop;
+
+#endif
--- generate/templates/templates/nodegit.cc
+++ generate/templates/templates/nodegit.cc
@@ -3,6 +3,7 @@
 #include <node.h>
 #include <git2.h>
 
+#include "../include/nodegit.h"
 #include "../include/wrapper.h"
 #include "../include/functions/copy.h"
 {% each %}
@@ -10,6 +11,8 @@
     #include "../include/{{ filename }}.h"
   {% endif %}
 {% endeach %}
+
+void run_libgit2_event_queue(void *);
 
 extern "C" void init(Local<v8::Object> target) {
   Nan::HandleScope scope;
@@ -20,6 +23,28 @@ extern "C" void init(Local<v8::Object> target) {
       {{ cppClassName }}::InitializeComponent(target);
     {% endif %}
   {% endeach %}
+
+  // initialize a thread on which we will execute all libgit2 calls
+  // for async NodeGit wrappers (and sync ones, eventually)
+  libgit2_loop = (uv_loop_t *)malloc(sizeof(uv_loop_t));
+  uv_loop_init(libgit2_loop);
+
+  uv_thread_t libgit2_thread_id;
+  uv_thread_create(&libgit2_thread_id, run_libgit2_event_queue, NULL);
 }
 
 NODE_MODULE(nodegit, init)
+
+uv_loop_t *libgit2_loop;
+
+void run_libgit2_event_queue(void *)
+{
+  // run, CPU, run
+  for ( ; ; ) {
+    uv_run(libgit2_loop, UV_RUN_DEFAULT);
+    // TODO: break out of loop
+  }
+
+  uv_loop_close(libgit2_loop);
+  free(libgit2_loop);
+}
