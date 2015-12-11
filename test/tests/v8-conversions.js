8a34381fdeeac8ef86db2dad1cee8bc50c9d4533
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-12-11 16:41:49 -0700

    moar testy [ci-skip]

--- test/tests/v8-conversions.js
+++ test/tests/v8-conversions.js
@@ -3,13 +3,22 @@ var assert = require("assert");
 describe.skip("Converting valuetypes in v8", function() {
   var nodegit = require("../..");
 
-  it("can accept truthy values for int-bools in setters", function() {
+  describe("integer-booleans (1s and 0s)", function() {
     var vals = [0, 1, 2, true, false, {}, "radishes", null, undefined, "", []];
 
-    vals.forEach(function (val) {
-      var opts = { ignoreCertErrors: val };
-      opts = new nodegit.CloneOptions(opts);
-      assert.equal(opts.ignoreCertErrors, !!val);
+    it("can accept truthy/falsey values in setters", function() {
+      vals.forEach(function (val) {
+        var opts = { ignoreCertErrors: val };
+        opts = new nodegit.CloneOptions(opts);
+      });
+    });
+
+    it("can retrieve true/false values via getters", function() {
+      vals.forEach(function (val) {
+        var opts = { ignoreCertErrors: val };
+        opts = new nodegit.CloneOptions(opts);
+        assert.equal(opts.ignoreCertErrors, !!val);
+      });
     });
   });
 });
