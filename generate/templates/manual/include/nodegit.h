f07b0989a0803dc143687cf7f67637db951af2f3
Stjepan Rajko <stjepanr@axosoft.com>
2015-12-16 18:13:09 -0700

    Add LockMaster

---
+++ generate/templates/manual/include/nodegit.h
@@ -0,0 +1,132 @@
+#ifndef NODEGIT_H
+#define NODEGIT_H
+
+#include <set>
+#include <vector>
+#include <uv.h>
+
+class LockMaster {
+
+  std::set<const void *> objects_to_lock;
+  std::vector<uv_mutex_t *> object_mutexes;
+
+  template<typename T>
+  void AddLocks(const T *t) {
+    // by default, don't lock anything
+  }
+
+  template<>
+  void AddLocks(const git_repository *repo) {
+    // when using a repo, lock the repo
+    objects_to_lock.insert(repo);
+  }
+
+  template<>
+  void AddLocks(const git_index *index) {
+    // when using an index, lock the repo, or if there isn't one lock the index
+    const void *owner = git_index_owner(index);
+    if(!owner) {
+      owner = index;
+    }
+    objects_to_lock.insert(owner);
+  }
+
+  // ... more locking rules would go here.  According to an analysis of idefs.json,
+  // the following types are passed as non-const * and may require locking
+  // (some likely, some probably not):
+  // 'git_annotated_commit',
+  // 'git_blame_options',
+  // 'git_blob',
+  // 'git_buf',
+  // 'git_checkout_options',
+  // 'git_cherrypick_options',
+  // 'git_clone_options',
+  // 'git_commit',
+  // 'git_config',
+  // 'git_diff',
+  // 'git_diff_perfdata',
+  // 'git_error',
+  // 'git_fetch_options',
+  // 'git_fetch_options',
+  // 'git_filter',
+  // 'git_filter_list',
+  // 'git_hashsig',
+  // 'git_index',
+  // 'git_merge_file_input',
+  // 'git_merge_options',
+  // 'git_merge_options',
+  // 'git_note',
+  // 'git_note_iterator',
+  // 'git_object',
+  // 'git_odb',
+  // 'git_odb_object',
+  // 'git_oid',
+  // 'git_oidarray',
+  // 'git_packbuilder',
+  // 'git_patch',
+  // 'git_pathspec',
+  // 'git_push_options',
+  // 'git_rebase',
+  // 'git_rebase_options',
+  // 'git_refdb',
+  // 'git_reference',
+  // 'git_reflog',
+  // 'git_remote',
+  // 'git_remote_callbacks',
+  // 'git_remote_callbacks',
+  // 'git_repository',
+  // 'git_repository_init_options',
+  // 'git_revwalk',
+  // 'git_signature',
+  // 'git_stash_apply_options',
+  // 'git_status_list',
+  // 'git_strarray',
+  // 'git_submodule',
+  // 'git_submodule_update_options',
+  // 'git_tag',
+  // 'git_transfer_progress',
+  // 'git_transport',
+  // 'git_tree',
+  // 'git_treebuilder',
+  // 'git_writestream'
+  //
+  // Other types are always passed as const * and perhaps don't require locking
+  // (it's not a guarantee though)
+
+  // base case for variadic template unwinding
+  void AddParameters()
+  {
+  }
+
+  // processes a single parameter, then calls recursively on the rest
+  template<typename T, typename... Types>
+  void AddParameters(const T *t, const Types*... args)
+  {
+    if(t) {
+      AddLocks(t);
+    } else {
+    }
+    AddParameters(args...);
+  }
+
+  void Lock();
+  void Unlock();
+
+public:
+
+  // we lock on construction
+  template<typename ...Types> LockMaster(bool emptyGuard, const Types*... types)
+  {
+    AddParameters(types...);
+    Lock();
+  }
+
+  // and unlock on destruction
+  ~LockMaster()
+  {
+    Unlock();
+  }
+
+};
+
+#endif
--- generate/templates/templates/nodegit.cc
+++ generate/templates/templates/nodegit.cc
@@ -2,7 +2,12 @@
 #include <v8.h>
 #include <node.h>
 #include <git2.h>
+#include <uv.h>
+#include <map>
+#include <algorithm>
+#include <set>
 
+#include "../include/nodegit.h"
 #include "../include/wrapper.h"
 #include "../include/functions/copy.h"
 {% each %}
@@ -10,6 +15,9 @@
     #include "../include/{{ filename }}.h"
   {% endif %}
 {% endeach %}
+
+std::map<const void *, uv_mutex_t *> mutexes;
+uv_mutex_t map_mutex;
 
 extern "C" void init(Local<v8::Object> target) {
   // Initialize libgit2.
@@ -23,6 +31,47 @@ extern "C" void init(Local<v8::Object> target) {
       {{ cppClassName }}::InitializeComponent(target);
     {% endif %}
   {% endeach %}
+
+  uv_mutex_init(&map_mutex);
+}
+
+std::vector<uv_mutex_t *> get_mutexes(const std::set<const void *> libgit2_objects)
+{
+  std::vector<uv_mutex_t *> result;
+
+  uv_mutex_lock(&map_mutex);
+
+  for (auto it = libgit2_objects.begin(); it != libgit2_objects.end(); it++) {
+    const void *object = *it;
+    if(object) {
+      // ensure we have an initialized mutex for each object
+      if(!mutexes[object]) {
+        mutexes[object] = (uv_mutex_t *)malloc(sizeof(uv_mutex_t));
+        uv_mutex_init(mutexes[object]);
+      }
+
+      result.push_back(mutexes[object]);
+    }
+  }
+
+  uv_mutex_unlock(&map_mutex);
+
+  return result;
+}
+
+void LockMaster::Lock()
+{
+  object_mutexes = get_mutexes(objects_to_lock);
+
+  // lock the mutex (note locking the mutexes one by one can lead to
+  // deadlocks... we might be better off locking them all at once
+  // (using trylock, then unlocking if they can't all be locked, then lock & wait, repeat...)
+  std::for_each(object_mutexes.begin(), object_mutexes.end(), uv_mutex_lock);
+}
+
+void LockMaster::Unlock()
+{
+  std::for_each(object_mutexes.begin(), object_mutexes.end(), uv_mutex_unlock);
 }
 
 NODE_MODULE(nodegit, init)
