952b6b21098676fe4e8d43ba662e6ab24c24d7f0
joseg <joseg@axosoft.com>
2016-02-29 13:09:48 -0700

    make sure test file name is correct on all OSs

--- test/tests/tree.js
+++ test/tests/tree.js
@@ -26,7 +26,8 @@ describe("Tree", function() {
     var repo = this.repository;
     var file1 = "test.txt";
     var file2 = "foo/bar.txt";
-    var expectedPaths = [file1, file2];
+    // index.addByPath doesn't like \s so normalize only for the expected paths
+    var expectedPaths = [file1, path.normalize(file2)];
     var progressEntries = [];
     var endEntries;
 
