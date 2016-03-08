50a620d7a7b42049b242a1cfb1f8812ebe24f088
Stjepan Rajko <stjepanr@axosoft.com>
2016-03-08 09:40:04 -0700

    Readd throttling

--- generate/templates/templates/struct_content.cc
+++ generate/templates/templates/struct_content.cc
@@ -17,7 +17,6 @@ extern "C" {
 #include "../include/lock_master.h"
 #include "../include/functions/copy.h"
 #include "../include/{{ filename }}.h"
-#include "../include/functions/sleep_for_ms.h"
 
 {% each dependencies as dependency %}
   #include "{{ dependency }}"
@@ -53,10 +52,7 @@ using namespace std;
     {% if not field.ignore %}
       {% if not field.isEnum %}
         {% if field.isCallbackFunction %}
-  if (this->{{ field.name }} != NULL) {
-    delete this->{{ field.name }};
     this->raw->{{ fields|payloadFor field.name }} = NULL;
-  }
         {% endif %}
       {% endif %}
     {% endif %}
@@ -84,7 +80,6 @@ void {{ cppClassName }}::ConstructFields() {
           // the current instance
           this->raw->{{ field.name }} = NULL;
           this->raw->{{ fields|payloadFor field.name }} = (void *)this;
-          this->{{ field.name }} = NULL;
         {% elsif field.payloadFor %}
 
           Local<Value> {{ field.name }} = Nan::Undefined();
