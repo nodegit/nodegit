4159f9a8b9407f0f0eaf07c8e844ec7c1ddda1d4
Chris Bargren <cbargren@gmail.com>
2016-04-29 11:37:27 -0700

    Fixing the install script so ESLint can run on it
     ESLint reads returns outside of a function block as a parser error, killing the linter on that file. While returning the promise that's running is more explicit, it isn't necessary to hold Node open while it finishes running, as Node doesn't finish a task until all of its callbacks have completed.

--- lifecycleScripts/install.js
+++ lifecycleScripts/install.js
@@ -11,98 +11,22 @@ try {
   fs.statSync(path.join(__dirname, "..", "src"));
   fs.statSync(path.join(__dirname, "..", "dist"));
   fromRegistry = true;
-}
-catch(e) {
+} catch (e) {
   fromRegistry = false;
-}
-
-if (!fromRegistry) {
-  console.info("[nodegit] Local install, no fetching allowed.");
-  return prepareAndBuild();
-}
-if (process.env.BUILD_DEBUG) {
-  console.info("[nodegit] Doing a debug build, no fetching allowed.");
-  return prepareAndBuild();
-}
-if (process.env.BUILD_ONLY) {
-  console.info("[nodegit] BUILD_ONLY is set to true, no fetching allowed.");
-  return prepareAndBuild();
-}
-
-return installPrebuilt();
-
-function installPrebuilt() {
-  console.info("[nodegit] Fetching binary from S3.");
-  var npg = pathForTool("node-pre-gyp");
-  return exec("\""+ npg + "\" install --fallback-to-build=false")
-    .then(
-      function() {
-        console.info("[nodegit] Completed installation successfully.");
-      },
-      function(err) {
-        console.info("[nodegit] Failed to install prebuilt binary:");
-        console.error(err);
-        console.info("[nodegit] Building manually. (You'll be here a while.)");
-        return prepareAndBuild();
-      }
-    );
 }
 
 function pathForTool(name) {
   var toolPath = path.resolve(".", "node_modules", ".bin", name);
-  if (process.platform == "win32") {
+  if (process.platform === "win32") {
     toolPath += ".cmd";
   }
   return toolPath;
 }
 
-function prepareAndBuild() {
-  console.info("[nodegit] Regenerating and configuring code");
-  return prepareForBuild()
-    .then(function() {
-      return build();
-    })
-    .then(function() {
-      return transpileJavascript();
-    });
-}
-
-function transpileJavascript() {
-  var cmd = pathForTool("babel");
-  var args = [
-    "--presets",
-    "es2015",
-    "-d",
-    "./dist",
-    "./lib"
-  ];
-  var opts = {
-    cwd: ".",
-    maxBuffer: Number.MAX_VALUE,
-    env: process.env,
-    stdio: "inherit"
-  };
-  var home = process.platform == "win32" ?
-              process.env.USERPROFILE : process.env.HOME;
-
-  opts.env.HOME = path.join(home, ".nodegit-gyp");
-
-  return new Promise(function(resolve, reject) {
-    var child = cp.spawn(cmd, args, opts);
-    child.on("close", function(code) {
-      if (code) {
-        reject(code);
-        process.exitCode = 13;
-      }
-      else {
-        resolve();
-      }
-    });
-  });
-}
-
 function build() {
+  /* eslint-disable no-console */
   console.info("[nodegit] Everything is ready to go, attempting compilation");
+  /* eslint-enable */
 
   var electronVersion = process.env.ELECTRON_VERSION;
   var nwjsVersion = process.env.NWJS_VERSION;
@@ -117,15 +41,14 @@ function build() {
   var debug = (process.env.BUILD_DEBUG ? "--debug" : "");
   var target = "";
   var arch = (process.env.TARGET_ARCH ?
-	"--arch=" + process.env.TARGET_ARCH : "");
+     "--arch=" + process.env.TARGET_ARCH : "");
   var distUrl = "";
   var runtime = "";
 
   process.argv.forEach(function(arg) {
     if (~arg.indexOf("electronVersion")) {
       electronVersion = arg.split("=")[1].trim();
-    }
-    else if (~arg.indexOf("nsjwVersion")) {
+    } else if (~arg.indexOf("nsjwVersion")) {
       nwjsVersion = arg.split("=")[1].trim();
     }
   });
@@ -134,15 +57,14 @@ function build() {
     target = "--target=" + electronVersion;
     distUrl = "--dist-url=https://atom.io/download/atom-shell";
     runtime = "--runtime=electron";
-  }
-  else if (nwjsVersion) {
+  } else if (nwjsVersion) {
     builder = "nw-gyp";
     target = "--target=" + nwjsVersion;
     runtime = "--runtime=node-webkit";
   }
 
-  var home = process.platform == "win32" ?
-              process.env.USERPROFILE : process.env.HOME;
+  var home = process.platform === "win32" ?
+    process.env.USERPROFILE : process.env.HOME;
 
   opts.env.HOME = path.join(home, ".nodegit-gyp");
 
@@ -162,14 +84,91 @@ function build() {
   return new Promise(function(resolve, reject) {
     var child = cp.spawn(cmd, args, opts);
     child.on("close", function(code) {
-      console.log(code);
+      console.log(code);  // eslint-disable-line no-console
       if (code) {
         reject(code);
         process.exitCode = 13;
-      }
-      else {
+      } else {
         resolve();
       }
     });
   });
 }
+
+function transpileJavascript() {
+  var cmd = pathForTool("babel");
+  var args = [
+    "--presets",
+    "es2015",
+    "-d",
+    "./dist",
+    "./lib"
+  ];
+  var opts = {
+    cwd: ".",
+    maxBuffer: Number.MAX_VALUE,
+    env: process.env,
+    stdio: "inherit"
+  };
+  var home = process.platform === "win32" ?
+    process.env.USERPROFILE : process.env.HOME;
+
+  opts.env.HOME = path.join(home, ".nodegit-gyp");
+
+  return new Promise(function(resolve, reject) {
+    var child = cp.spawn(cmd, args, opts);
+    child.on("close", function(code) {
+      if (code) {
+        reject(code);
+        process.exitCode = 13;
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function prepareAndBuild() {
+  /* eslint-disable no-console */
+  console.info("[nodegit] Regenerating and configuring code");
+  /* eslint-enable */
+  return prepareForBuild()
+    .then(function() {
+      return build();
+    })
+    .then(function() {
+      return transpileJavascript();
+    });
+}
+
+/* eslint-disable no-console */
+function installPrebuilt() {
+  console.info("[nodegit] Fetching binary from S3.");
+  var npg = pathForTool("node-pre-gyp");
+  return exec("\"" + npg + "\" install --fallback-to-build=false")
+    .then(
+      function() {
+        console.info("[nodegit] Compvared installation successfully.");
+      },
+      function(err) {
+        console.info("[nodegit] Failed to install prebuilt binary:");
+        console.error(err);
+        console.info("[nodegit] Building manually. (You'll be here a while.)");
+        return prepareAndBuild();
+      }
+    );
+}
+
+if (!fromRegistry) {
+  console.info("[nodegit] Local install, no fetching allowed.");
+  prepareAndBuild();
+} else if (process.env.BUILD_DEBUG) {
+  console.info("[nodegit] Doing a debug build, no fetching allowed.");
+  prepareAndBuild();
+} else if (process.env.BUILD_ONLY) {
+  console.info("[nodegit] BUILD_ONLY is set to true, no fetching allowed.");
+  prepareAndBuild();
+} else {
+  installPrebuilt();
+}
+/* eslint-enable */
