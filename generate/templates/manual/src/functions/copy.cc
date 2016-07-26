cdc5767e5a1a94a2726e85513d2a2e2bdfb4a5c2
Kyle Smith <kyles@axosoft.com>
2016-07-26 10:48:25 -0700

    Memory management: duplicate time
    it's unsafe to use the signature's time directly since the signature could be freed. Instead duplicate the signature's time when it is requested and set it as self freeing.

--- generate/templates/manual/src/functions/copy.cc
+++ generate/templates/manual/src/functions/copy.cc
@@ -11,6 +11,11 @@ const git_error *git_error_dup(const git_error *arg) {
   return result;
 }
 
+void git_time_dup(git_time **out, const git_time *arg) {
+  *out = (git_time *)malloc(sizeof(git_time));
+  memcpy(*out, arg, sizeof(git_time));
+}
+
 void git_transfer_progress_dup(git_transfer_progress **out, const git_transfer_progress *arg) {
   *out = (git_transfer_progress *)malloc(sizeof(git_transfer_progress));
   memcpy(*out, arg, sizeof(git_transfer_progress));
