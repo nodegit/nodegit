0f53a5199adc7ec0a326d56b7d625e6f5f12c884
Remy Suen <remy.suen@gmail.com>
2017-12-21 07:20:43 +0900

    `ceiling_dirs` parameter in `Repository.discover` is optional
    libgit2's git_repository_discover function's ceiling_dirs parameter
    can be null. Flag the parameter as such in the JSON file so that the
    NodeGit wrapper API behaves the same way.
    
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- test/tests/repository.js
+++ test/tests/repository.js
@@ -200,13 +200,23 @@ describe("Repository", function() {
     });
   });
 
-  it("can discover if a path is part of a repository", function() {
+  function discover(ceiling) {
     var testPath = path.join(reposPath, "lib", "util", "normalize_oid.js");
     var expectedPath = path.join(reposPath, ".git");
-    return NodeGit.Repository.discover(testPath, 0, "")
+    return NodeGit.Repository.discover(testPath, 0, ceiling)
       .then(function(foundPath) {
         assert.equal(expectedPath, foundPath);
       });
+  }
+
+  it("can discover if a path is part of a repository, null ceiling",
+      function() {
+    return discover(null);
+  });
+
+  it("can discover if a path is part of a repository, empty ceiling",
+      function() {
+    return discover("");
   });
 
   it("can create a repo using initExt", function() {
