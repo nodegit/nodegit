58063a589f80b68718caa94be32b1271c1c4bfb5
Tyler Wanek <tylerw@axosoft.com>
2018-09-09 15:19:03 -0700

    Continue using OpenSSL over libgcrypt on windows

--- vendor/libssh2/win32/libssh2_config.h
+++ vendor/libssh2/win32/libssh2_config.h
@@ -18,7 +18,7 @@
 #define HAVE_GETTIMEOFDAY
 #endif /* __MINGW32__ */
 
-#define HAVE_LIBCRYPT32
+#define LIBSSH2_OPENSSL
 #define HAVE_WINSOCK2_H
 #define HAVE_IOCTLSOCKET
 #define HAVE_SELECT
@@ -44,4 +44,3 @@
 #define LIBSSH2_DH_GEX_NEW 1
 
 #endif /* LIBSSH2_CONFIG_H */
-
