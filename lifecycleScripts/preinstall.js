e3c95e14e169c94d139ff27110b06a407354f42d
Tyler Wanek <tylerw@axosoft.com>
2019-02-21 10:30:22 -0700

    If npm -v fails, we should assume we're in yarn and do nothing

--- lifecycleScripts/preinstall.js
+++ lifecycleScripts/preinstall.js
@@ -8,14 +8,22 @@ module.exports = function prepareForBuild() {
   console.log("[nodegit] Running pre-install script");
 
   return exec("npm -v")
-    .then(function(npmVersion) {
-      if (npmVersion.split(".")[0] < 3) {
-        console.log("[nodegit] npm@2 installed, pre-loading required packages");
-        return exec("npm install --ignore-scripts");
-      }
+    .then(
+      function(npmVersion) {
+        if (npmVersion.split(".")[0] < 3) {
+          console.log(
+            "[nodegit] npm@2 installed, pre-loading required packages"
+          );
+          return exec("npm install --ignore-scripts");
+        }
 
-      return Promise.resolve();
-    })
+        return Promise.resolve();
+      },
+      function() {
+        // We're installing via yarn, so don't
+        // care about compability with npm@2
+      }
+    )
     .then(function() {
       if (buildFlags.isGitRepo) {
         var submodules = require(local("submodules"));
