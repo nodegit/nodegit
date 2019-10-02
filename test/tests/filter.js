bd3f48cc52ec22b5aa0221a81af901ad045be3c8
Mia <mia.chen.mtl@gmail.com>
2019-10-01 21:48:31 -0400

    Fix Tree.prototype.entryById and .getEntry * entryById now follows pattern of entryByName and entryByIndex where the parent property is added to the entry. * entryByPath renamed to _entryByPath since getEntry should be used instead. * Simple test for entryById added. * Setup of commits used in test for TreeEntry.walk moved to test setup so getEntry can test searching by path as well. * Tests using Tree.prototype.entryByPath now use Tree.prototype.getEntry.

--- test/tests/filter.js
+++ test/tests/filter.js
@@ -1065,7 +1065,7 @@ describe("Filter", function() {
           return commit.getTree();
         })
         .then(function(tree) {
-          return tree.entryByPath("README.md");
+          return tree.getEntry("README.md");
         })
         .then(function(entry) {
           return test.repository.getBlob(entry.id());
