22d94f1f637c52fb593a047f78227eb6054430c6
Ian Hattendorf <ianh@axosoft.com>
2020-01-13 16:27:48 -0700

    Format opts.cc

--- generate/templates/manual/libgit2/opts.cc
+++ generate/templates/manual/libgit2/opts.cc
@@ -2,8 +2,7 @@ NAN_METHOD(GitLibgit2::Opts)
 {
   Nan::EscapableHandleScope scope;
 
-  if (info.Length() == 0 || !info[0]->IsNumber())
-  {
+  if (info.Length() == 0 || !info[0]->IsNumber()) {
     return Nan::ThrowError("Number option is required.");
   }
 
@@ -12,127 +11,108 @@ NAN_METHOD(GitLibgit2::Opts)
   git_error_clear();
 
   v8::Local<v8::Value> to;
-  switch (from_option)
-  {
-  // GET size_t
-  case GIT_OPT_GET_MWINDOW_SIZE:
-  case GIT_OPT_GET_MWINDOW_MAPPED_LIMIT:
-  case GIT_OPT_GET_PACK_MAX_OBJECTS:
-  {
-    size_t option_value;
-    if (git_libgit2_opts(from_option, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+  switch (from_option) {
+    // GET size_t
+    case GIT_OPT_GET_MWINDOW_SIZE:
+    case GIT_OPT_GET_MWINDOW_MAPPED_LIMIT:
+    case GIT_OPT_GET_PACK_MAX_OBJECTS: {
+      size_t option_value;
+      if (git_libgit2_opts(from_option, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(option_value);
+      break;
     }
-    to = Nan::New<Number>(option_value);
-    break;
-  }
-  // GET int
-  case GIT_OPT_GET_WINDOWS_LONGPATHS:
-  {
-    int option_value;
-    if (git_libgit2_opts(from_option, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    // GET int
+    case GIT_OPT_GET_WINDOWS_LONGPATHS: {
+      int option_value;
+      if (git_libgit2_opts(from_option, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(option_value);
+      break;
     }
-    to = Nan::New<Number>(option_value);
-    break;
-  }
-  // GET unsigned long
-  case GIT_OPT_GET_WINDOWS_SHAREMODE:
-  {
-    unsigned long option_value;
-    if (git_libgit2_opts(from_option, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    // GET unsigned long
+    case GIT_OPT_GET_WINDOWS_SHAREMODE: {
+      unsigned long option_value;
+      if (git_libgit2_opts(from_option, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(option_value);
+      break;
     }
-    to = Nan::New<Number>(option_value);
-    break;
-  }
-  // GET ssize_t
-  case GIT_OPT_GET_CACHED_MEMORY:
-  {
-    ssize_t option_value;
-    if (git_libgit2_opts(from_option, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    // GET ssize_t
+    case GIT_OPT_GET_CACHED_MEMORY: {
+      ssize_t option_value;
+      if (git_libgit2_opts(from_option, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(option_value);
+      break;
     }
-    to = Nan::New<Number>(option_value);
-    break;
-  }
-  // GET git_buf
-  case GIT_OPT_GET_TEMPLATE_PATH:
-  case GIT_OPT_GET_USER_AGENT:
-  {
-    git_buf option_value = {0};
-    if (git_libgit2_opts(from_option, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    // GET git_buf
+    case GIT_OPT_GET_TEMPLATE_PATH:
+    case GIT_OPT_GET_USER_AGENT: {
+      git_buf option_value = { 0 };
+      if (git_libgit2_opts(from_option, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<v8::String>(option_value.ptr, option_value.size)
+             .ToLocalChecked();
+      git_buf_dispose(&option_value);
+      break;
     }
-    to = Nan::New<v8::String>(option_value.ptr, option_value.size).ToLocalChecked();
-    git_buf_dispose(&option_value);
-    break;
-  }
-  case GIT_OPT_GET_SEARCH_PATH:
-  {
-    git_buf option_value = {0};
-    if (info.Length() < 2 || !info[1]->IsNumber())
-    {
-      return Nan::ThrowError("Number option is required.");
+    case GIT_OPT_GET_SEARCH_PATH: {
+      git_buf option_value = { 0 };
+      if (info.Length() < 2 || !info[1]->IsNumber()) {
+        return Nan::ThrowError("Number option is required.");
+      }
+      int level = (int)info[1].As<v8::Number>()->Value();
+      if (git_libgit2_opts(from_option, level, &option_value)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<v8::String>(option_value.ptr, option_value.size)
+             .ToLocalChecked();
+      git_buf_dispose(&option_value);
+      break;
     }
-    int level = (int)info[1].As<v8::Number>()->Value();
-    if (git_libgit2_opts(from_option, level, &option_value))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    // SET int
+    case GIT_OPT_ENABLE_CACHING:
+    case GIT_OPT_ENABLE_STRICT_OBJECT_CREATION:
+    case GIT_OPT_ENABLE_STRICT_SYMBOLIC_REF_CREATION:
+    case GIT_OPT_ENABLE_OFS_DELTA:
+    case GIT_OPT_ENABLE_FSYNC_GITDIR:
+    case GIT_OPT_ENABLE_STRICT_HASH_VERIFICATION:
+    case GIT_OPT_ENABLE_UNSAVED_INDEX_SAFETY:
+    case GIT_OPT_DISABLE_PACK_KEEP_FILE_CHECKS:
+    case GIT_OPT_SET_WINDOWS_LONGPATHS: {
+      if (info.Length() < 2 || !info[1]->IsNumber()) {
+        return Nan::ThrowError("Number option is required.");
+      }
+      int option_arg = (int)info[1].As<v8::Number>()->Value();
+      if (git_libgit2_opts(from_option, option_arg)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(0);
+      break;
     }
-    to = Nan::New<v8::String>(option_value.ptr, option_value.size).ToLocalChecked();
-    git_buf_dispose(&option_value);
-    break;
-  }
-  // SET int
-  case GIT_OPT_ENABLE_CACHING:
-  case GIT_OPT_ENABLE_STRICT_OBJECT_CREATION:
-  case GIT_OPT_ENABLE_STRICT_SYMBOLIC_REF_CREATION:
-  case GIT_OPT_ENABLE_OFS_DELTA:
-  case GIT_OPT_ENABLE_FSYNC_GITDIR:
-  case GIT_OPT_ENABLE_STRICT_HASH_VERIFICATION:
-  case GIT_OPT_ENABLE_UNSAVED_INDEX_SAFETY:
-  case GIT_OPT_DISABLE_PACK_KEEP_FILE_CHECKS:
-  case GIT_OPT_SET_WINDOWS_LONGPATHS:
-  {
-    if (info.Length() < 2 || !info[1]->IsNumber())
-    {
-      return Nan::ThrowError("Number option is required.");
+    // SET size_t
+    case GIT_OPT_SET_MWINDOW_SIZE:
+    case GIT_OPT_SET_MWINDOW_MAPPED_LIMIT:
+    case GIT_OPT_SET_PACK_MAX_OBJECTS: {
+      if (info.Length() < 2 || !info[1]->IsNumber()) {
+        return Nan::ThrowError("Number option is required.");
+      }
+      size_t option_arg = (size_t)info[1].As<v8::Number>()->Value();
+      if (git_libgit2_opts(from_option, option_arg)) {
+        return Nan::ThrowError("git_libgit2_opts failed");
+      }
+      to = Nan::New<Number>(0);
+      break;
     }
-    int option_arg = (int)info[1].As<v8::Number>()->Value();
-    if (git_libgit2_opts(from_option, option_arg))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
+    default: {
+      return Nan::ThrowError("Unsupported option");
     }
-    to = Nan::New<Number>(0);
-    break;
-  }
-  // SET size_t
-  case GIT_OPT_SET_MWINDOW_SIZE:
-  case GIT_OPT_SET_MWINDOW_MAPPED_LIMIT:
-  case GIT_OPT_SET_PACK_MAX_OBJECTS:
-  {
-    if (info.Length() < 2 || !info[1]->IsNumber())
-    {
-      return Nan::ThrowError("Number option is required.");
-    }
-    size_t option_arg = (size_t)info[1].As<v8::Number>()->Value();
-    if (git_libgit2_opts(from_option, option_arg))
-    {
-      return Nan::ThrowError("git_libgit2_opts failed");
-    }
-    to = Nan::New<Number>(0);
-    break;
-  }
-  default:
-  {
-    return Nan::ThrowError("Unsupported option");
-  }
   }
 
   return info.GetReturnValue().Set(scope.Escape(to));
