fec768fa4332166a0ffd4b2240aa14b47725a2b4
Julien Chaumond <chaumond@gmail.com>
2020-10-09 20:48:37 +0200

    Update details-for-tree-entry.js

--- examples/details-for-tree-entry.js
+++ examples/details-for-tree-entry.js
@@ -1,29 +1,26 @@
-var nodegit = require("../");
-var path = require("path");
+const nodegit = require("../");
+const path = require("path");
 
 /**
  * This shows how to get details from a tree entry or a blob
 **/
 
-nodegit.Repository.open(path.resolve(__dirname, "../.git"))
-  .then(function(repo) {
-    return repo.getTree("e1b0c7ea57bfc5e30ec279402a98168a27838ac9")
-    .then(function(tree) {
-      var treeEntry = tree.entryByIndex(0);
+(async () => {
+  const repo = await nodegit.Repository.open(path.resolve(__dirname, "../.git"));
+  const tree = await repo.getTree("e1b0c7ea57bfc5e30ec279402a98168a27838ac9");
+  const treeEntry = tree.entryByIndex(0);
+  
+  // Tree entry doesn't have any data associated with the actual entry
+  // To get that we need to get the index entry that this points to
+  const index = await repo.refreshIndex();
+  const indexEntry = index.getByPath(treeEntry.path());
 
-      // Tree entry doesn't have any data associated with the actual entry
-      // To get that we need to get the index entry that this points to
-      return repo.refreshIndex().then(function(index) {
-        var indexEntry = index.getByPath(treeEntry.path());
+  // With the index entry we can now view the details for the tree entry
+  console.log("Entry path: " + indexEntry.path);
+  console.log("Entry time in seconds: " + indexEntry.mtime.seconds());
+  console.log("Entry oid: " + indexEntry.id.toString());
+  console.log("Entry size: " + indexEntry.fileSize);
+  
+  console.log("Done!");
+})();
 
-        // With the index entry we can now view the details for the tree entry
-        console.log("Entry path: " + indexEntry.path);
-        console.log("Entry time in seconds: " + indexEntry.mtime.seconds());
-        console.log("Entry oid: " + indexEntry.id.toString());
-        console.log("Entry size: " + indexEntry.fileSize);
-      });
-    });
-  })
-  .done(function() {
-    console.log("Done!");
-  });
