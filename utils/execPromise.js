ad6f35cbbdb961fc3e5e71ac7617e6fd610a755c
Ian Hattendorf <ianh@axosoft.com>
2021-10-18 17:39:08 -0700

    Optionally pipe exec output to stdout/stderr

--- utils/execPromise.js
+++ utils/execPromise.js
@@ -3,9 +3,9 @@ var cp = require('child_process');
 // We have to manually promisify this because at this is required in lifecycle
 // methods and we are not guaranteed that any 3rd party packages are installed
 // at this point
-module.exports = function(command, opts) {
+module.exports = function(command, opts, extraOpts = {}) {
   return new Promise(function(resolve, reject) {
-    return cp.exec(command, opts, function(err, result) {
+    const childProcess = cp.exec(command, opts, function(err, result) {
       if (err) {
         reject(err);
       }
@@ -13,5 +13,12 @@ module.exports = function(command, opts) {
         resolve(result);
       }
     });
+
+    if (extraOpts.pipeOutput) {
+      childProcess.stdout.pipe(process.stdout);
+      childProcess.stderr.pipe(process.stderr);
+    }
+
+    return childProcess;
   });
 };
