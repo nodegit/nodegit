06b90ef9eb4d979e9b2f52363144925e78ba68d8
Julian Mesa <julian.mesa@gitkraken.com>
2022-01-11 17:27:23 +0100

    Fix a memcmp errors in code that was changed from memmem to memcmp (#656 )
    Notes:
    Fixed supported algo prefs list check when upgrading rsa keys
    
    Credit: Michael Buckley

--- vendor/libssh2/src/userauth.c
+++ vendor/libssh2/src/userauth.c
@@ -1147,7 +1147,7 @@ _libssh2_key_sign_algorithm(LIBSSH2_SESSION *session,
             f = strchr(a, ',');
             f_len = f ? (f - a) : (int) strlen(a);
 
-            if(f_len == p_len && memcmp(a, s, p_len)) {
+            if(f_len == p_len && memcmp(a, s, p_len) == 0) {
 
                 if(i != filtered_algs) {
                     memcpy(i, ",", 1);
@@ -1185,7 +1185,7 @@ _libssh2_key_sign_algorithm(LIBSSH2_SESSION *session,
             f = strchr(a, ',');
             f_len = f ? (f - a) : (int) strlen(a);
 
-            if(f_len == p_len && memcmp(a, s, p_len)) {
+            if(f_len == p_len && memcmp(a, s, p_len) == 0) {
                 /* found a match, upgrade key method */
                 match = s;
                 match_len = p_len;
