5926ba183a2e2dfbec00cfca87e890a0ac2805fe
Julian Mesa <julian.mesa@gitkraken.com>
2022-10-20 11:33:01 +0200

    Fix git_diff_line leak

--- generate/templates/manual/include/functions/free.h
+++ generate/templates/manual/include/functions/free.h
@@ -8,5 +8,6 @@
 #define NODEGIT_FREE_FUNCTIONS
 
 void git_remote_head_free(git_remote_head *remote_head);
+void git_diff_line_free(const git_diff_line *diff_line);
 
 #endif
--- generate/templates/manual/src/functions/free.cc
+++ generate/templates/manual/src/functions/free.cc
@@ -7,3 +7,8 @@ void git_remote_head_free(git_remote_head *remote_head) {
   free(remote_head->symref_target);
   free(remote_head);
 }
+
+void git_diff_line_free(const git_diff_line *diff_line) {
+  free((void*)(diff_line->content));
+  free((void*) diff_line);
+}
